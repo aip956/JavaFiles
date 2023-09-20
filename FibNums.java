ca07685d69a84ed0e8059f9452437d13dec3adf2
Anthea Ip <antheaip964@gmail.com>
2023-09-19 19:18:33 -0700

    Fib nums

--- FibNums.java
+++ FibNums.java
@@ -2,7 +2,15 @@
 
 
 public class FibNums {
-    {
+    public static long[] printFibb(int n){
         //Your code here
+    }
+
+    public static void main(String[] args) {
+        int n1 = 5;
+        System.out.println("Output1: " + (printFibb(n1)));
+        int n2 = 5;
+        System.out.println("Output1: " + (printFibb(n2)));
+        
     }
 }
