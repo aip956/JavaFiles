7306dbd9147871601e8e176f1a8771f1b721ecab
Anthea Ip <antheaip964@gmail.com>
2023-09-19 19:28:29 -0700

    Saving FibNums

--- FibNums.java
+++ FibNums.java
@@ -4,6 +4,17 @@
 public class FibNums {
     public static long[] printFibb(int n){
         //Your code here
+        Array<Long> fibNums = new Array<>();
+        if (n == 1) {
+            fibNums = {1};
+        }
+        else if (n >= 2) {
+            fibNums = {1, 1};
+            for (int i = 2; i < n; i++) {
+                fibNums[i] = {fibNums[i - 2] + fibNums [i - 1]}
+            }
+        }
+        return fibNums;
     }
 
     public static void main(String[] args) {
