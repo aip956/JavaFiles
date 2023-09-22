160de4e2165720b02124d1283584cf365818a265
Anthea Ip <antheaip964@gmail.com>
2023-09-22 19:08:13 -0400

    Saving equil point

--- equilPoint.java
+++ equilPoint.java
@@ -1,8 +1,18 @@
+// https://practice.geeksforgeeks.org/problems/equilibrium-point-1587115620/1
+
 public class equilPoint {
     public static int equilibriumPoint(long arr[], int n) {
         // get sum of arr
         // start a left sum, subtract each element from total
         // if leftSum == total; that's the equil point!
+         // Total Array Sum = 1,3,5,2,2 = 13
+        // Sub elem from Total; 
+        // if leftsum == total; return ind+1
+        // leftsum + elem
+        // leftsum = 0
+        // total-1=12; leftsum+1=1
+        // total-3=9; leftsum+3=4
+        // 9-5 = 4; 
 
         long arrSum = 0;
         long leftSum = 0;
