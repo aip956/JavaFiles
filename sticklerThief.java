f0232b62114bf485b0a6fd5669659b659cd4b4dc
Anthea Ip <antheaip964@gmail.com>
2023-09-23 14:03:19 -0400

    Stickler Thief

---
+++ sticklerThief.java
@@ -0,0 +1,14 @@
+public class sticklerThief {
+    public int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+    }
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
