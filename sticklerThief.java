00114818a0e40a9ba4fd08c94e8532d4e9511022
Anthea Ip <antheaip964@gmail.com>
2023-09-25 12:20:43 -0400

    Saving stickler Theif

--- sticklerThief.java
+++ sticklerThief.java
@@ -1,10 +1,20 @@
 // https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1
+/*
+ Create array to store max money at i'th house
 
+
+ */
 
 public class sticklerThief {
     public int FindMaxSum(int arr[], int n)
     {
         // Your code here
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+        // Create array to store max looted amount to ith house
+        int[] dp = new int[n];
+
     }
 
 
@@ -15,7 +25,7 @@ public class sticklerThief {
         int n1 = 5;
         int[] a2 = {1, 5, 3};
         int n2 = 3;
-        
+
     }
     
 }
