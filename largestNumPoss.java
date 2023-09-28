f082f480a4494999d926b4096ea34a3698e059e1
Anthea Ip <antheaip964@gmail.com>
2023-09-28 12:34:51 -0400

    Largest Num Possible

---
+++ largestNumPoss.java
@@ -0,0 +1,13 @@
+// https://practice.geeksforgeeks.org/problems/largest-number-possible5028/1
+// Given two numbers "N" and "S", find the largest number that can be formed
+// with N digits that sum to "S". Return -1 if not possible
+// Ex: N = 2; S = 9 => 90
+
+
+public class largestNumPoss {
+    static String findLargest(int N, int S){
+        // code here
+    }
+
+    
+}
