4b279729e39474ce78448d8d3156dbbb7a2cc77c
Anthea Ip <antheaip964@gmail.com>
2023-09-28 13:12:16 -0400

    Largest sum

--- largestNumPoss.java
+++ largestNumPoss.java
@@ -2,12 +2,19 @@
 // Given two numbers "N" and "S", find the largest number that can be formed
 // with N digits that sum to "S". Return -1 if not possible
 // Ex: N = 2; S = 9 => 90
+// for 
 
 
-public class largestNumPoss {
+public static class largestNumPoss {
     static String findLargest(int N, int S){
         // code here
     }
 
-    
+
+    public static void main (String[] args) {
+        int n1 = 2;
+        int s1 = 9;
+        System.out.println("Output1: " + findLargest(n1, s1));
+    }
+
 }
