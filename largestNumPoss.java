a3aa47b421d210f634b90a185277eee80c6025dc
Anthea Ip <antheaip964@gmail.com>
2023-09-28 15:09:08 -0400

    Saving

--- largestNumPoss.java
+++ largestNumPoss.java
@@ -1,17 +1,25 @@
-// https://practice.geeksforgeeks.org/problems/largest-number-possible5028/1
-// Given two numbers "N" and "S", find the largest number that can be formed
-// with N digits that sum to "S". Return -1 if not possible
-// Ex: N = 2; S = 9 => 90
-// for 
-
+/* https://practice.geeksforgeeks.org/problems/largest-number-possible5028/1
+Given two numbers "N" and "S", find the largest number that can be formed
+with N digits that sum to "S". Return -1 if not possible
+ Ex: N = 2; S = 9 => 90
+30; 9; 9; 9; 
+Ex: N = 2; S = 4 => 40
+*/
 
 public static class largestNumPoss {
     static String findLargest(int N, int S){
         // code here; if N = 2, S = 100; can't be done; 99 < 100
         // Stringbuild 9 while < S
         if (N * 9 < S) return -1; // need if statement if can't be done
-        
-        for ()
+        // 9 + 10*9
+        StringBuilder output = new StringBuilder();
+        int sum;
+        while (sum < S) {
+            sum += 9;
+            output.append(9);
+        }
+        int diff = S - sum;
+        output.append(diff);
     }
 
 
