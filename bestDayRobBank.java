3497bfee7dd2badc1766e54cd92af93f90d826c4
Anthea Ip <antheaip964@gmail.com>
2023-09-29 10:02:48 -0400

    Best day to rob bank

---
+++ bestDayRobBank.java
@@ -0,0 +1,16 @@
+public class bestDayRobBank {
+    public static int[] goodDaysToRobBank(int[] security, int time) {
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] security1 = {5,3,3,3,5,6,2};
+        int time1 = 2;
+        System.out.println("Output1: " + goodDaysToRobBank(security1, time1));
+    }
+
+
+}
