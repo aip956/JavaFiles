a409df4f44ad0caaf07fd03d00c3b483f84d680b
Anthea Ip <antheaip964@gmail.com>
2023-09-30 15:09:47 -0400

    Saving

--- bestDayRobBank.java
+++ bestDayRobBank.java
@@ -2,27 +2,48 @@
 
 
 
-
+import java.util.*;
 public class bestDayRobBank {
-    public List<Integer> goodDaysToRobBank(int[] security, int time) {
-        ArrayList<Integer> output = new ArrayList<>();
+    public static List<Integer> goodDaysToRobBank(int[] security, int time) {
+        List<Integer> output = new ArrayList<>();
+        int[] numDaysBefore = new int[security.length];
+        int[] numDaysAfter = new int[security.length];
+
         if (security.length < 1 + (2*time)) {
             return output;
         }
         // have to consider time is large
 
-        for (int i = time; i < time-1; i++) {
-            if (security[i-time] > security[i-time+1]) {
-                break;
+        // iterate left to right for numDaysBefore
+        for (int i = 1; i < security.length; i++) {
+            if (security[i] <= security[i-1]) {
+                numDaysBefore[i] = 1 + numDaysBefore[i-1];
             }
-            for (int i = time; i < security.length - time; i++) {
-                if (security[i] > security[i+1]) {
-                    break;
-                }
+            System.out.println("i: " + i);
+            System.out.println("numDaysBefore[i]: " + numDaysBefore[i]);
+            // System.out.println("numDaysBefore[i-1]: " + numDaysBefore[i-1]);
+        }
+        // iterate left to right for numDaysAfter
+        for (int i = security.length - 2; i >= 0; i--) {
+            if (security[i] <= security[i+1]) {
+                numDaysAfter[i] = 1 + numDaysAfter[i+1];
             }
-            ouput
+            System.out.println("i: " + i);
+            System.out.println("numDaysAFter[i]: " + numDaysAfter[i]);
+
         }
         
+        // iterate over time windows
+        for (int i = time; i < security.length - time; i++) {
+            // System.out.println("36: " + i);
+            System.out.println("time: " + time);
+            System.out.println("numDaysBefore[i]: " + numDaysBefore[i]);
+            System.out.println("numDaysAfter[i]: " + numDaysAfter[i]);
+            if (numDaysBefore[i] >= time && numDaysAfter[i] >= time) {
+ 
+                output.add(i);
+            }
+        }
         return output;
     }
 
