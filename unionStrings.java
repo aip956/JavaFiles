cdbad43f495edc0fc5e7156bb1a1a9b2ab2a1eed
Anthea Ip <antheaip964@gmail.com>
2023-10-01 11:50:53 -0400

    Union strings

--- unionStrings.java
+++ unionStrings.java
@@ -14,9 +14,20 @@
 
 import java.util.*;
 public class unionStrings {
-    public static String my_union(String str1, String str2) {
+    public static void my_union(String str1, String str2) {
         ArrayList<Character> uniqueChars = new ArrayList<>();
-        
+        for (char letter : str1.toCharArray()) {
+            if (!uniqueChars.contains(letter)) {
+                uniqueChars.add(letter);
+            }
+        }
+        for (char letter : str2.toCharArray()) {
+            if (!uniqueChars.contains(letter)) {
+                uniqueChars.add(letter);
+            }
+        }
+        System.out.println("24uniqueChars AL: " + uniqueChars);
+
     }
     
 
@@ -24,20 +35,20 @@ public class unionStrings {
         String input1a = "zpadinton";
         String input1b = "paqefwtdjetyiytjneytjoeyjnejeyj";
         my_union(input1a, input1b);
-        String expected1 = "zpadintoqefwjy";
-        boolean compare1 = expected1.equals(my_union(input1a, input1b));
-        System.out.println(compare1);
+        // String expected1 = "zpadintoqefwjy";
+        // boolean compare1 = expected1.equals(my_union(input1a, input1b));
+        // System.out.println(compare1);
 
-        String input2a = "ddf6vewg64f";
-        String input2b = "gtwthgdwthdwfteewhrtag6h4ffdhsd";
-        String expected2 = "df6vewg4thras";
-        boolean compare2 = expected2.equals(my_union(input2a, input2b));
-        System.out.println(compare2);
+        // String input2a = "ddf6vewg64f";
+        // String input2b = "gtwthgdwthdwfteewhrtag6h4ffdhsd";
+        // String expected2 = "df6vewg4thras";
+        // boolean compare2 = expected2.equals(my_union(input2a, input2b));
+        // System.out.println(compare2);
 
-        String input3a = "rien";
-        String input3b = "cette phrase ne cache rien";
-        String expected3 = "rienct phas";
-        boolean compare3 = expected3.equals(my_union(input3a, input3b));
-        System.out.println(compare3);
+        // String input3a = "rien";
+        // String input3b = "cette phrase ne cache rien";
+        // String expected3 = "rienct phas";
+        // boolean compare3 = expected3.equals(my_union(input3a, input3b));
+        // System.out.println(compare3);
     }
 }
