f729c9c5b1c766db2f584aedd844c7aea154da8c
Anthea Ip <antheaip964@gmail.com>
2023-10-02 09:26:21 -0400

    Closest Pair from 2 arrays

---
+++ closestPairFrom2Arrs.java
@@ -0,0 +1,19 @@
+// https://practice.geeksforgeeks.org/problems/find-the-closest-pair-from-two-arrays4215/1
+
+import java.util.*;
+public class closestPairFrom2Arrs {
+
+    public static ArrayList<Integer> printClosest (int arr[], int brr[], int n, int m, int x) {
+        // code here
+    }
+
+
+    public static void main(String[] args) {
+        int arr1[] = {1, 4, 5, 7};
+        int brr1[] = {10, 20, 30, 40};
+        int n1 = 4;
+        int m1 = 4;
+        System.out.println("Output1: " printClosest(arr1, brr1, n1, m1))
+    }
+}
+
