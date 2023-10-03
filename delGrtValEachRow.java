40b567abd55435238ec6e8aa34168e52dd5c971e
Anthea Ip <antheaip964@gmail.com>
2023-10-03 12:58:21 -0400

    Saving

--- delGrtValEachRow.java
+++ delGrtValEachRow.java
@@ -21,47 +21,52 @@ subArr lenghth is less one
 
 class delGrtValEachRow {
     public static int deleteGreatestValue(int[][] grid) {
-        int m = grid.length; // 2
-        int n = grid[0].length; // 3
+        int m = grid.length; // 2; num of rows
+        int n = grid[0].length; // 3; row length, num of cols
+        System.out.println("26n: " + n);
         int summedMax = 0;
+        // iterate through each subArrs
+        while (n > 0)  { // for both rows
+            int arrMax = -1; // max between subArrs
+            // iterate through row
+            for (int i = 0; i < m; i++) {
+                int subArrMax = -1; // max within this subarray
+                for (int j = 0; j < n; j++) { // for one row
+                    System.out.println("35n: " + n);
+                    subArrMax = Math.max(subArrMax, grid[i][j]);
+                    System.out.println("i: " + i + ", j: " + j + " , subArrMax: " + subArrMax);
+                    
+                    arrMax = Math.max(arrMax, subArrMax);
+                    System.out.println("i: " + i + ", j: " + j + " , arrMax: " + arrMax);
+                }
+                System.out.println("40arrMax: " + arrMax);
 
-        int arrMax = -1; // max between subArrs
+                
 
-    // iterate through each subArrs
-    for (int i = 0; i < n; i++)  { // for both rows
-        int subArrMax = -1; // max within this subarray
-        System.out.println("i: " + i );
-        for (int j = 0; j < m; j++) { // for one row
-            subArrMax = Math.max(subArrMax, grid[i][j]);
-            System.out.println("i,j,subArrMax: " + i + " "+ j + " " + subArrMax);
-            arrMax = Math.max(arrMax, subArrMax);
-            System.out.println("i,j,arrMax: " + i + " "+ j + " " + arrMax);
-        }
-        System.out.println("40arrMax: " + arrMax);
-        summedMax += arrMax;
-        System.out.println("42summedMax: " + summedMax);
-    }
+                    // go back through row; find subArrMax and shift elements after to left
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == subArrMax) {
+                        for (int k = j; k < n - 1; k++) {
+                            grid[i][k] = grid[i][k+1];
+                        }
+                        
+                        break;
+                    }
+                }
+                
+            }// for row
+            n--;
+            summedMax += arrMax;
+            System.out.println("42summedMax: " + summedMax);
+        } // while
         System.out.println("44summedMax: " + summedMax);
         return summedMax;
-        
-  }
-
-    public static int[] removeElem (int[] row, int elem) {
-        int[] newRow = new int[row.length - 1];
-        int index = 0;
-        for (int i = 0; i < row.length; i++) {
-            if (row[i] != elem) {
-                newRow[index] = row[i];
-                index++;
-            }
-        }
-        return newRow;
     }
+
 
     public static void main(String[] args) {
         int[][] grid = {{1,2,4}, {3,3,1}};
         int output = deleteGreatestValue(grid);
         System.out.println(output);
     }
-
 }
