00edd717b62753fa8d365ff4c23094284b3f6a8a
Anthea Ip <antheaip964@gmail.com>
2023-10-04 14:37:12 -0400

    Task Scheduler

---
+++ taskScheduler.java
@@ -0,0 +1,21 @@
+/* https://leetcode.com/problems/task-scheduler/description/
+ * 
+ * 
+ * 
+ */
+
+
+
+public class taskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        char[] tasks1 = { "A","A","A","B","B","B" };
+        int n1 = 2;
+        System.out.println("Output1: " + leastInterval(tasks1, n1));
+    }
+}
