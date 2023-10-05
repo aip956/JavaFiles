4196da8b922e2a9cf812310e2bc5bc233f54b422
Anthea Ip <antheaip964@gmail.com>
2023-10-05 17:26:21 -0400

    Passes

--- taskScheduler.java
+++ taskScheduler.java
@@ -9,6 +9,10 @@
  * (Total time < length of iarray, there are additional tasks that can be inserted
  *  between the most freq tasks to meet cooldown req.)
  * 
+ * Time: O(N): N is length of taskss array; iterate through tasks once to count freq
+ * of each task, then iterate through taskCount array size 26
+ * 
+ * Space is O(26) or O(1) because used fixed size array taskCount of length 26
  * 
  */
 
@@ -23,30 +27,28 @@ public class taskScheduler {
         for (char task : tasks) {
             if (task >= 'A' && task <= 'Z') {
                 taskCount[task - 'A']++;
-                System.out.println("task: " + task);
-                System.out.println("taskCount[task]: " + taskCount[task - 'A']);
+                // System.out.println("task: " + task);
+                // System.out.println("taskCount[task]: " + taskCount[task - 'A']);
             }           
         }
 
         // Find the task with the highest freq
         for (int count : taskCount) {
             maxFreq = Math.max(maxFreq, count);
-            System.out.println("count: " + count);
-            System.out.println("maxFreq: " + maxFreq);
+            // System.out.println("count: " + count);
+            // System.out.println("maxFreq: " + maxFreq);
         }
 
         // Calc total time without cooldown
-        int totalTime = (maxFreq - 1) * (n + 1) + maxFreq;
-        System.out.println("totalTime: " + totalTime);
-
-        // Check if there are other tasks with same freq
+        int countMaxFreq = 0;
         for (int count : taskCount) {
             if (count == maxFreq) {
-                totalTime++;
-                System.out.println("46count: " + count);
-                System.out.println("totalTime: " + totalTime);
+                countMaxFreq++;
             }
         }
+        int totalTime = (maxFreq - 1) * (n + 1) + countMaxFreq;
+        System.out.println("totalTime: " + totalTime);
+
         System.out.println("50totalTime: " + totalTime);
         System.out.println("51tasks.length: " + tasks.length);
         return Math.max(totalTime, tasks.length);
@@ -58,5 +60,13 @@ public class taskScheduler {
         char[] tasks1 = {'A','A','A','B','B','B'};
         int n1 = 2;
         System.out.println("Output1: " + leastInterval(tasks1, n1));
+
+        char[] tasks2 = {'A','A','A','B','B','B'};
+        int n2 = 0;
+        System.out.println("Output2: " + leastInterval(tasks2, n2));
+
+        char[] tasks3 = {'A','A','A','A','A','A','B','C','D','E','F','G'};
+        int n3 = 2;
+        System.out.println("Output3: " + leastInterval(tasks3, n3));
     }
 }
