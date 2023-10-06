ffb2a502b54c60adaf79442cd35fe501f748333c
Anthea Ip <antheaip964@gmail.com>
2023-10-06 12:31:07 -0400

    Saving

--- rearrangeArray.java
+++ rearrangeArray.java
@@ -1,3 +1,8 @@
+/* https://leetcode.com/problems/array-with-elements-not-equal-to-average-of-neighbors/description/
+ * sort array
+ * 
+ */
+
 public class rearrangeArray {
     public int[] rearrangeArray(int[] nums) {
         
@@ -7,5 +12,7 @@ public class rearrangeArray {
         int nums1 = {1,2,3,4,5};
         System.out.println("Output1: " + rearrangeArray(nums1));
 
+        int nums2 = {6,2,0,9,7};
+        System.out.println("Output2: " + rearrangeArray(nums2));
     }
 }
