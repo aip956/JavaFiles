92ca2a68ab225157373b99cf2ad84b691ed26976
Anthea Ip <antheaip964@gmail.com>
2023-10-06 13:46:44 -0400

    Saving

--- rearrangeArray.java
+++ rearrangeArray.java
@@ -1,10 +1,22 @@
 /* https://leetcode.com/problems/array-with-elements-not-equal-to-average-of-neighbors/description/
+ * 
+ * Take 1/2 values of array: 1 _ 2 _ 3, then put remaining nums in remaining slots
+ * 1 _ 4 _ 2 _ 5 _ 3; each num is surrounded by nums higher or lower so wont be average
  * sort array
+ * res = []
+ * while length or result != result;
+ * l, r = 0; len(nums) - 1
  * 
  */
-
+import java.util.*;
 public class rearrangeArray {
     public int[] rearrangeArray(int[] nums) {
+        int[] result = [];
+        int left = 0; 
+        int right = 0;
+        int length = nums.length;
+        Collections.sort(nums);
+
         
     }
 
