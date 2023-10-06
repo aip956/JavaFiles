0fd59a526502eceab11356c7801b44a116f762ad
Anthea Ip <antheaip964@gmail.com>
2023-10-06 12:28:38 -0400

    Rearrange Array

---
+++ rearrangeArray.java
@@ -0,0 +1,11 @@
+public class rearrangeArray {
+    public int[] rearrangeArray(int[] nums) {
+        
+    }
+
+    public static void main(String[] args) {
+        int nums1 = {1,2,3,4,5};
+        System.out.println("Output1: " + rearrangeArray(nums1));
+
+    }
+}
