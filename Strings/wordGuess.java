c53be5f7657f749709977b84abb8650db2d4b5f6
Anthea Ip <antheaip964@gmail.com>
2023-10-08 15:07:03 -0400

    saving

--- Strings/wordGuess.java
+++ Strings/wordGuess.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class wordGuess {
     public static void main(String[] args){
-        Scanner letter = new Scanner(System.in);  
+        Scanner scanner = new Scanner(System.in);  
         System.out.println("Enter a letter");
-        guess = user_input.next();
+        char guess = scanner.next().charAt(0);
         System.out.println(guess);
+
+        scanner.close();
     }
 
 }
