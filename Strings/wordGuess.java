b1ca941804cb9786c50140a7196fbc201fa665d8
Anthea Ip <antheaip964@gmail.com>
2023-10-08 15:14:37 -0400

    Saving

--- Strings/wordGuess.java
+++ Strings/wordGuess.java
@@ -2,10 +2,20 @@ import java.util.*;
 
 public class wordGuess {
     public static void main(String[] args){
+        String[] wordsArr = {"apple, queue, mayor, nicer, story"};
+        Random random = new Random();
+        int randomNum = random.nextInt(5);
+        String gameWord = wordsArr[randomNum];
+        
         Scanner scanner = new Scanner(System.in);  
+        int max_guesses = 7;
+        int guessInWord = 0;
+        while (max_guesses <= 7 && guessInWord < 5)
+
         System.out.println("Enter a letter");
         char guess = scanner.next().charAt(0);
         System.out.println(guess);
+
 
         scanner.close();
     }
