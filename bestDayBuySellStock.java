5216e9bcf4ac7174b270e32b26d887267c172617
Anthea Ip <antheaip964@gmail.com>
2023-10-13 17:57:39 -0400

    Saving

--- bestDayBuySellStock.java
+++ bestDayBuySellStock.java
@@ -2,5 +2,14 @@
 
 
 public class bestDayBuySellStock {
-    
+    public static int maxProfit(int[] prices) {
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] prices1 = {7,1,5,3,6,4};
+        System.out.println("Output1: " + maxProfit(prices1));
+    }
 }
