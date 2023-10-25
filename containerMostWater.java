f7c84ab8dafe1a521a45f736836d2acceb76e2ed
Anthea Ip <antheaip964@gmail.com>
2023-10-25 13:32:27 -0400

    Saving

--- containerMostWater.java
+++ containerMostWater.java
@@ -1,3 +1,11 @@
+// https://leetcode.com/problems/container-with-most-water/
+/* 1. Ceate a var to return final number
+ * 2. Have 2 pointers at each end of the array
+ * 3. maxArea = height * length
+ * 4. Find smallest side aka height; min height of the two heights
+ * 5. length = length of array - 1
+ */
+
 public class containerMostWater {
     public static int maxArea(int[] height) {
         System.out.println(height[0]);
