28b787dd71800b211b692794147e5edda9a543ff
Anthea Ip <antheaip964@gmail.com>
2023-10-26 19:04:22 -0400

    Saving

--- addArrayToNum.java
+++ addArrayToNum.java
@@ -3,7 +3,21 @@ import java.util.List;
 public class addArrayToNum {
     
     public List<Integer> addToArrayForm(int[] num, int k) {
-            
+        // convert num to a number; mult each num by 10 and add
+        // add k
+        // convert back to array    
+        
+
+    }
+
+    public integer convertToNum(int[] num) {
+        // 2, 3, 4
+        int number = 0;
+        for (int el : num) {
+            number = (el * 10) + number;
+            System.out.println("number: " + number);
+        } 
+
     }
 
 
