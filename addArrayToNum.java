46d5ad1e0e645b4e5eba1d2d646f60362b520c66
Anthea Ip <antheaip964@gmail.com>
2023-10-26 18:59:56 -0400

    Add array to num

---
+++ addArrayToNum.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class addArrayToNum {
+    
+    public List<Integer> addToArrayForm(int[] num, int k) {
+            
+    }
+
+
+    public static void main(String[] args) {
+        int[] A = {1,2,0,0};
+        int K = 34;
+        System.out.println("Output: " + addArrayToForm(A, K));
+    }
+
+
+}
