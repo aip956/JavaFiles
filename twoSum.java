0e73a1fdc7a9e47fee27f529044d5bf7078b8cf0
Anthea Ip <antheaip964@gmail.com>
2023-11-01 19:43:54 -0400

    Saving

---
+++ twoSum.java
@@ -0,0 +1,17 @@
+public class twoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        // iterate through nums
+        // for each element, is the compliment in a hash
+        // if not put it index, compliment
+
+        for (int num : nums) {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2,7,11,15};
+        int target1 = 9;
+        System.out.println(twoSum(nums1, target1));
+    }
+}
