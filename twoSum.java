d68dc07a8694198fe3e808831bf6dcc1d31a918e
Anthea Ip <antheaip964@gmail.com>
2023-11-01 20:44:00 -0400

    Saving

--- twoSum.java
+++ twoSum.java
@@ -5,21 +5,34 @@ public class twoSum {
         // iterate through nums
         // for each element, is the compliment in a hash
         // if not put it index, compliment
-        HashMap<Integer, Integer> compHash = new HashMap<>();
+        // round 1: ind = 0, num[i] = 2, compl = 7; add to hash: {2, 0} or num[i],i
+        // round 2: ind = 1, num[i] = 7, compl = 2; add to hash: {2, 0}
+
+        Map<Integer, Integer> compHash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int compliment = target - nums[i];
+            System.out.println("Compl: " + compliment);
             if (compHash.containsKey(compliment)) {
                 return new int[] {compHash.get(compliment), i};
             } else {
-                compHash.put(i, compliment);
+                compHash.put(nums[i], i);
             }
             System.out.println("Hash: " + compHash);
         }
+        return new int[] {};
     }
 
     public static void main(String[] args) {
         int[] nums1 = {2,7,11,15};
         int target1 = 9;
-        System.out.println(twoSum(nums1, target1));
+        int[] result1 = twoSum(nums1, target1);
+        System.out.print("Output1: [");
+        for (int i = 0; i < result1.length; i++) {
+            System.out.print(result1[i]);
+            if (i < result1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
