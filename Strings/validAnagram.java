5ade0d738388180ee3ddb1030bf0f7658d9d9776
Anthea Ip <antheaip964@gmail.com>
2023-11-08 17:37:03 -0500

    Saving

--- Strings/validAnagram.java
+++ Strings/validAnagram.java
@@ -6,10 +6,17 @@ public class validAnagram {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
         Map<Character, Integer> charFreq = new HashMap<>();
-        for (Character letter : s) {
-            charFreq.put(letter, charFreq.getOrDefault(charFreq, null))
+        // create map for s
+        for (Character letter : s.toCharArray()) {
+            charFreq.put(letter, charFreq.getOrDefault(letter, 0) + 1);
         }
-
+        // decrement for t
+        for (Character letter : t.toCharArray()) {
+            if (!charFreq.containsKey(letter)) {
+                return false;
+            }
+            
+        }
         
     }
 
@@ -23,6 +30,12 @@ public class validAnagram {
 
 /*
  * Determine that s.length() == t.length()
+ * Create a var to store str chars frequ
+ * loop throug str1 and fill in obj with key being char and value being freq
+ * loop throug hstr2 and if char is not in the Obj, return alse
+ * if char is in obj, decease value by one
+ * if extra char or not enough, return false
+ * 
  * Create a hash table for s, and one for t
  * Determine if they are equal
  * decrease each occurence from t in s
