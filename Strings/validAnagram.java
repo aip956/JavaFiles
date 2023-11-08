997cc8fd025e6da9646d2b4e3ecf59dd3115ccd1
Anthea Ip <antheaip964@gmail.com>
2023-11-08 17:14:36 -0500

    Saving

--- Strings/validAnagram.java
+++ Strings/validAnagram.java
@@ -1,5 +1,16 @@
+// https://leetcode.com/problems/valid-anagram/
+import java.util.*;
 package Strings;
 
 public class validAnagram {
+    public boolean isAnagram(String s, String t) {
+        
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println("Output: " + isAnagram(s, t);
+    }
     
 }
