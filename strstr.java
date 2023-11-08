a80ca968bf00505015355f94e33c6846e7caa268
Anthea Ip <antheaip964@gmail.com>
2023-11-07 21:23:19 -0500

    saving

--- strstr.java
+++ strstr.java
@@ -1,33 +1,42 @@
 public class strstr {
-    public static int findStrstr(String s, String x) {
-        // Geeksforgeeks; fr
-        // mississippi, 11; issi, 4
-        int lenS = s.length();
-        int lenX = x.length();
-        x.toCharArray();
-        s.toCharArray();
-        int i;
+    // public static int findStrstr(String s, String x) {
+    //     // Geeksforgeeks; fr
+    //     // mississippi, 11; issi, 4
+    //     int lenS = s.length();
+    //     int lenX = x.length();
+    //     x.toCharArray();
+    //     s.toCharArray();
+    //     int i;
    
-        //  mississippi, 11; issi, 4
-        //  iterate 11 - 4 = 7
-        // set boolean = true
-        // iterate over lenght of substring
-        // if string.charAt i+j != substr.charAt j, 
-        // set to false, move to next string letter 
-        // if they equal, keep iterating through lenX
-        for (i = 0; i <= lenS - lenX; i++) {
-            boolean found = true;
-            for (int j = 0; j < lenX; j++) {
-                if (s.charAt(i+j) != x.charAt(j)) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) {
-                return i;
-            }
-        }
-        return -1;
+    //     //  mississippi, 11; issi, 4
+    //     //  iterate 11 - 4 = 7
+    //     // set boolean = true
+    //     // iterate over lenght of substring
+    //     // if string.charAt i+j != substr.charAt j, 
+    //     // set to false, move to next string letter 
+    //     // if they equal, keep iterating through lenX
+    //     for (i = 0; i <= lenS - lenX; i++) {
+    //         boolean found = true;
+    //         for (int j = 0; j < lenX; j++) {
+    //             if (s.charAt(i+j) != x.charAt(j)) {
+    //                 found = false;
+    //                 break;
+    //             }
+    //         }
+    //         if (found) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+    public static int findStrstr(String s, String x) {
+        // string length of s and x
+        // to charArray on both
+        // iterate on s-x length, if s[i] = x[i+j]
+        // boolean found = true
+        // iterate through 
     }
 
 
