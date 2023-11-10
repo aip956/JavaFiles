cf155cb2848838831595471a60d4ee6c7ac43d75
Anthea Ip <antheaip964@gmail.com>
2023-11-10 14:09:57 -0500

    Save

--- isItOneBit.java
+++ isItOneBit.java
@@ -28,7 +28,55 @@ public class isItOneBit {
         System.out.println("Is it a 1-bit: " + isOneBitCharacter(bits3));
 
       }
-
-
-
 }
+/*
+ * We have two special characters. The first character can be represented by one
+ * bit 0. The second character can be represented by two bits (10 or 11).
+ * Now given a string represented by several bits. Return whether the last
+ * character must be a one-bit character or not. The given string will always
+ * end with a zero.
+ * 
+ * Example 00:
+ * Input:
+ * bits = [1, 0, 0]
+ * Output: True
+ ** Explanation:**
+ * The only way to decode it is two-bit character and one-bit character. So the
+ * last character is one-bit character.
+ * 
+ * Example 01:
+ * Input:
+ * bits = [1, 1, 1, 0]
+ * Output: False
+ ** Explanation:**
+ * The only way to decode it is two-bit character and two-bit character. So the
+ * last character is NOT one-bit character.
+ * Note:
+ * 
+ * 1 <= len(bits) <= 1000.
+ * bits[i] is always 0 or 1.
+ */
+
+/*
+ * [1,1,0,1,0]
+ * element type: 0, 1
+ * number of elements:
+ * last element is 1; part of two bit
+ * last element i 0:
+ * 2nd to last ele:
+ * length of the array is even: could be 2x2 bits or 4x1, but if any 1s, need to
+ * be part of 2 bit
+ * if i sum the array, vs. the length, array length even: sum has to be even
+ * array length even:
+ * if sum of arr is odd: false
+ * if summ of arr is even: true
+ * array length is odd:
+ * if sum of arr is odd: true
+ * X if summ of arr is even: false
+ * 
+ * from left, el = 1, then el[]+1 makes the 2-bit
+ * boolean: 2bit?
+ * for length of array,
+ * if array[i] == 1,
+ * 
+ */
