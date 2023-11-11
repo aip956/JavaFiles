f34d56ad777d6c523a7ddd0b67706d73ede7d84e
Anthea Ip <antheaip964@gmail.com>
2023-11-11 14:27:13 -0500

    Save

--- destinationCity.java
+++ destinationCity.java
@@ -1,24 +1,41 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class destinationCity {
-        public String destCity(List<List<String>> paths) {
-            Set<String> dest = new HashSet<>();
-            for (int i = 0; i < paths.length; i++) {
-                Set.put(paths[i][0]);
+        public static String destCity(List<List<String>> paths) {
+            Set<String> citySet = new HashSet<>();
+            for (int i = 0; i < paths.size(); i++) {
+                citySet.add(paths.get(i).get(0));
             }
-    }
+
+            for (int i = 0; i < paths.size(); i++) {
+                if (!citySet.contains(paths.get(i).get(1))) {
+                    return paths.get(i).get(1);
+                }
+            }
+        return "-1";
+        }
 
 
     public static void main(String[] args) {
-        List<List<String>> paths1 = {{"B","C"},{"D","B"},{"C","A"}};
+        List<List<String>> paths1 = new ArrayList<>();
+        paths1.add(new ArrayList<>(List.of("B","C")));
+        paths1.add(new ArrayList<>(List.of("D","B")));
+        paths1.add(new ArrayList<>(List.of("C","A")));
+        
+        List<List<String>> paths2 = new ArrayList<>();
+        paths2.add(new ArrayList<>(List.of("London","New York")));
+        paths2.add(new ArrayList<>(List.of("New York","Lima")));
+        paths2.add(new ArrayList<>(List.of("Lima","Sao Paulo")));
 
-        String[][] paths2 = {{"London","New York"},{"New York","Lima"},{"Lima","Sao Paulo"}};
-        String[][] paths3 = {{"A","Z"}};
-        System.out.println("Destination1: " + destCity(paths1));
-        System.out.println("Destination2: " + destCity(paths2));
-        System.out.println("Destination3: " + destCity(paths3));
+        List<List<String>> paths3 = new ArrayList<>();
+        paths3.add(new ArrayList<>(List.of("A","Z")));
+        
+        System.out.println("Destination1: " + destCity(paths1)); // A
+        System.out.println("Destination2: " + destCity(paths2)); // Sao Paulo
+        System.out.println("Destination3: " + destCity(paths3)); // Z
     }
 }
 
@@ -31,4 +48,7 @@ add paths[i][0] to set
 iterate through paths[i][1]
 if path[i][1] is in set, keep going
 if not in set, return path[i][1]
+
+Time complexity: Loops through twice, O(2n) => O(n)
+Space complexity: Created a set, O(n)
  * */
