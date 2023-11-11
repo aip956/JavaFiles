d46ae4037ed28fb27f9d5c612b62b60770fee93a
Anthea Ip <antheaip964@gmail.com>
2023-11-11 14:10:55 -0500

    Save

--- destinationCity.java
+++ destinationCity.java
@@ -1,13 +1,24 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class destinationCity {
         public String destCity(List<List<String>> paths) {
-        
+            Set<String> dest = new HashSet<>();
+            for (int i = 0; i < paths.length; i++) {
+                Set.put(paths[i][0]);
+            }
     }
 
 
     public static void main(String[] args) {
-        String[] paths1 = {{"B","C"},{"D","B"},{"C","A"}};
+        List<List<String>> paths1 = {{"B","C"},{"D","B"},{"C","A"}};
+
+        String[][] paths2 = {{"London","New York"},{"New York","Lima"},{"Lima","Sao Paulo"}};
+        String[][] paths3 = {{"A","Z"}};
+        System.out.println("Destination1: " + destCity(paths1));
+        System.out.println("Destination2: " + destCity(paths2));
+        System.out.println("Destination3: " + destCity(paths3));
     }
 }
 
