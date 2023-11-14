df40201c5533a65f944e3781b1ea993582dfac58
Anthea Ip <antheaip964@gmail.com>
2023-11-13 19:04:10 -0500

    Save

--- groupAnag.java
+++ groupAnag.java
@@ -6,29 +6,41 @@
 import java.util.*;
 
 public class groupAnag {
+    // public static List<List<String>> groupAnagrams(String[] strs) {
+    //     Map<String, List<String>> map = new HashMap<>();
+
+    //     for (String word : strs) {
+    //         // make a char[] from the word
+    //         char[] chars = word.toCharArray();
+    //         // sort letters in the word
+    //         Arrays.sort(chars);
+    //         // make a string from the chars[]; bat => abt
+    //         String sortedWord = new String(chars);
+
+    //         // if the map does not contain the sorted word, add it
+    //         if (!map.containsKey(sortedWord)) {
+    //             map.put(sortedWord, new ArrayList<>());
+    //             System.out.println("23map: " + map);
+    //         }
+    //         // add the word based on it's sorted version
+    //         map.get(sortedWord).add(word);
+    //         System.out.println("27map: " + map);
+    //     }
+    //     return new ArrayList<>(map.values());
+    // }
+
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
+        // word in the strs array, sorted word
+        // create charArray for each word => sorted word
+        // if sorted word is in the wordMap, add based on sorted version
 
+        Map<String> strsMap = new HashMap<>();
         for (String word : strs) {
-            // make a char[] from the word
-            char[] chars = word.toCharArray();
-            // sort letters in the word
-            Arrays.sort(chars);
-            // make a string from the chars[]; bat => abt
-            String sortedWord = new String(chars);
-
-            // if the map does not contain the sorted word, add it
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
-                System.out.println("23map: " + map);
-            }
-            // add the word based on it's sorted version
-            map.get(sortedWord).add(word);
-            System.out.println("27map: " + map);
+            char[] wordCharArray = word.toCharArray();
+            System.out.println(wordCharArray);
         }
-        return new ArrayList<>(map.values());
-    }
 
+    }
 
     public static void main(String[] args) {
         String[] strs = {"eat","tea","tan","ate","nat","bat"};
