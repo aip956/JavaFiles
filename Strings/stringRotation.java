ff350d2c070257ea4a8f7492cd74ab2021addf5a
Anthea Ip <antheaip964@gmail.com>
2023-11-14 13:41:31 -0500

    Save

---
+++ Strings/stringRotation.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class stringRotation {
+        public static boolean areRotations(String s1, String s2 )
+    {
+        // Your code here
+        
+    }
+
+    public static void main(String[] args) {
+        String s1 = "geeksforgeeks";
+        String s2 = "forgeeksgeeks";
+        System.out.println("Output1: " +areRotations(s1,s2));
+    }
+}
