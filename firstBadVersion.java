d041462d5ddda1ba8dcfa94f522e62a0c9a0a31c
Anthea Ip <antheaip964@gmail.com>
2023-11-16 13:16:23 -0500

    Save

---
+++ firstBadVersion.java
@@ -0,0 +1,17 @@
+public class firstBadVersion {
+    public int firstBadVersion(int n) {
+        // 
+        
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int bad = 4;
+        System.out.println("Output1: " + firstBadVersion(n, bad));
+    }
+}
+
+
+/*
+ *  https://leetcode.com/problems/first-bad-version/
+ */
