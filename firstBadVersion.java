752f1a3848ba949d8f41eab5a766c70db3b5473b
Anthea Ip <antheaip964@gmail.com>
2023-11-16 13:33:20 -0500

    Save

--- firstBadVersion.java
+++ firstBadVersion.java
@@ -1,13 +1,24 @@
 public class firstBadVersion {
     public int firstBadVersion(int n) {
-        // 
+        // divide n by 2; that is the pivot; isBadVersion(mid) = false; number is good
+        // if mid = true, mid = mid / 2
+        // if mid = false, mid = (mid + bad) / 2
+        int mid = n / 2;
+        System.out.println("mid: " + mid);
+        while (!isBadVersion(mid))
+        if (!isBadVersion(mid)) {
+            System.out.println("mid is good: " + !isBadVersion(mid));
+            mid = mid / 2;
+        } else {
+            System.out.println("mid is good: " + !isBadVersion(mid));
+            mid = (n+mid) / 2;
+        }
         
     }
 
     public static void main(String[] args) {
         int n = 5;
-        int bad = 4;
-        System.out.println("Output1: " + firstBadVersion(n, bad));
+        System.out.println("Output1: " + firstBadVersion(n));
     }
 }
 
