eddf6b39b591f5b7bb625c0502b093acde2a6e8a
Anthea Ip <antheaip964@gmail.com>
2023-11-25 13:57:41 -0500

    Save

---
+++ printPattern.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class printPattern {
+        public List<Integer> pattern(int N){
+        // code here
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
