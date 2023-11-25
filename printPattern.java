84fe016605c32b841e1b38b0f1fd430fa7a15738
Anthea Ip <antheaip964@gmail.com>
2023-11-25 14:00:04 -0500

    Save

--- printPattern.java
+++ printPattern.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class printPattern {
         public List<Integer> pattern(int N){
         // code here
+        List<Integer> list = new ArrayList<>();
+        
+
     }
 
     public static void main(String[] args) {
