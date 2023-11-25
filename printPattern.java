ce089eacd2d3fd2be8678071ed9ca3d6c0e779b5
Anthea Ip <antheaip964@gmail.com>
2023-11-25 14:05:38 -0500

    Save

--- printPattern.java
+++ printPattern.java
@@ -5,7 +5,7 @@ public class printPattern {
         public List<Integer> pattern(int N){
         // code here
         List<Integer> list = new ArrayList<>();
-        
+        list.add(N);
 
     }
 
@@ -14,3 +14,15 @@ public class printPattern {
         System.out.println("Output1: " + pattern(n1));
     }
 }
+
+
+/*
+ * https://www.geeksforgeeks.org/problems/print-pattern3549/1
+ * Print a sequence of numbers starting with N, without using 
+ * loop, where replace N with N - 5, until N > 0. After that 
+ * replace N with N + 5 until N regains its initial value.
+ * 
+ * use recursive
+ * if n > 0, add n-5 to list; decrease n by 5
+ * 
+ */
