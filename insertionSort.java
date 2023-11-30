0a8da6f020778f4e23df320f70a1c51ecd954f48
Anthea Ip <antheaip964@gmail.com>
2023-11-29 23:26:50 -0500

    Save

---
+++ insertionSort.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public List<List<Pair>> insertionSort(List<Pair> pairs) {
+
+    }
+
+    public static void main(String[] args) {
+        Pair[] pairs = { new Pair(5, "apple"), new Pair(2, "banana"), new Pair(9, "cherry")};
+
+    }
+}
