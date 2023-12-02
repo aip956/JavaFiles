3873f506f95b4529b150c81938ca9966c080f37e
Anthea Ip <antheaip964@gmail.com>
2023-12-02 13:11:58 -0500

    Save

---
+++ excelSheetColumn.java
@@ -0,0 +1,40 @@
+public class excelSheetColumn {
+    
+}
+
+
+
+
+/*
+
+n = 1, ascii = 65, result = 'A'
+
+ * Topic: Excel Sheet Column Title
+
+Given a positive integer, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+ 1 -> A 2 -> B 3 -> C ... 26 -> Z 27 -> AA 28 -> AB ... 
+
+
+Example 0:
+Input: 1
+Output: "A" 
+
+	Example 1:
+Input: 28
+Output: "AB" 
+
+Example 2:
+Input: 701
+Output: ZY
+
+
+Function Prototype in JS (you can do this in any language)
+function excel_sheet_column_title(param_1)
+{
+
+}
+
+ * 
+ */
