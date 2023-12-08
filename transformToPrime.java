4e48952b4cb4be335d2f4f914807b68598dc60d4
Anthea Ip <antheaip964@gmail.com>
2023-12-08 12:48:36 -0500

    Saving

---
+++ transformToPrime.java
@@ -0,0 +1,14 @@
+public class transformToPrime {
+    public static int minNumber(int arr[], int N)
+    {
+
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        int arr[] = {2, 4, 6, 8, 12};
+        System.out.println("Output: " + minNumber(arr, N));
+    }
+
+
+}
