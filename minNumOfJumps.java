27834c2e0ad7621324caa4580cdd705be5c3e9ae
Anthea Ip <antheaip964@gmail.com>
2023-12-11 12:52:39 -0500

    Saving

--- minNumOfJumps.java
+++ minNumOfJumps.java
@@ -4,7 +4,7 @@ public class minNumOfJumps {
         int n = arr.length;
         if (n <= 1) return 0;
         if (arr[0] == 0) return -1;
-
+// {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
         int maxReach = arr[0];
         int steps = arr[0];
         int jumps = 1;
