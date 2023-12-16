d36390095baa62df43b89dba94d40aa595f16c0f
Anthea Ip <antheaip964@gmail.com>
2023-12-16 12:49:38 -0500

    Save

---
+++ strstr.java
@@ -0,0 +1,74 @@
+public class strstr {
+    // public static int findStrstr(String s, String x) {
+    //     // Geeksforgeeks; fr
+    //     // mississippi, 11; issi, 4
+    //     int lenS = s.length();
+    //     int lenX = x.length();
+    //     x.toCharArray();
+    //     s.toCharArray();
+    //     int i;
+   
+    //     //  mississippi, 11; issi, 4
+    //     //  iterate 11 - 4 = 7
+    //     // set boolean = true
+    //     // iterate over lenght of substring
+    //     // if string.charAt i+j != substr.charAt j, 
+    //     // set to false, move to next string letter 
+    //     // if they equal, keep iterating through lenX
+    //     for (i = 0; i <= lenS - lenX; i++) {
+    //         boolean found = true;
+    //         for (int j = 0; j < lenX; j++) {
+    //             if (s.charAt(i+j) != x.charAt(j)) {
+    //                 found = false;
+    //                 break;
+    //             }
+    //         }
+    //         if (found) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+    public static int findStrstr(String s, String x) {
+        // string length of s and x
+        // to charArray on both
+        // iterate on s-x length, if s[i] = x[i+j]
+        // boolean found = true
+        // iterate through subarray, if !=, found = false to jump out of loop
+
+        int lenS = s.length();
+        int lenX = x.length();
+        s.toCharArray();
+        x.toCharArray();
+        for (int i = 0; i < (lenS - lenX); i++) {
+            boolean found = true;
+            for (int j = 0; j < lenX; j++) {
+                if (s.charAt(i+j) != x.charAt(j)) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+        // s = GeeksForGeeks, x = Fr
+        // String b = "geeksquiz";
+        // String s = "GeeksForGeeks";
+        // String x = "Fr";
+        String s = "Mississippi";
+        String x = "issip";
+        Integer output = findStrstr(s, x);
+        System.out.println("Output: " + output); // Output: 
+    }
+}
+
+
