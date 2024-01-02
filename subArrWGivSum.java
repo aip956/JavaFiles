ae9b7fd2550b13bad91ce5ee95fb57097d29a82b
Anthea Ip <antheaip964@gmail.com>
2024-01-02 17:52:05 -0500

    Saving

--- subArrWGivSum.java
+++ subArrWGivSum.java
@@ -1,9 +1,26 @@
 // https://www.geeksforgeeks.org/find-subarray-with-given-sum/
+
 /*
+https://www.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1?page=1&sortBy=submissions
 Traverse the array from start to end.
 From every index start another loop from i to the end of the array to get all subarrays starting from i, and keep a variable currentSum to calculate the sum of every subarray.
 For every index in inner loop update currentSum = currentSum + arr[j]
 If the currentSum is equal to the given sum then print the subarray.
+
+Given an unsorted array A of size N that contains only non negative integers, find a continuous sub-array that adds to a given number S and return the left and right index(1-based indexing) of that subarray.
+
+In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.
+
+Note:- You have to return an ArrayList consisting of two elements left and right. In case no such subarray exists return an array consisting of element -1.
+
+Example 1:
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
  */
 
 import java.util.ArrayList;
