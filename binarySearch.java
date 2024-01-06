3558dd2ce11d70efaecc8bdb6ebf63cafe1d5d0b
Anthea Ip <antheaip964@gmail.com>
2024-01-06 13:43:27 -0500

    Saving

--- binarySearch.java
+++ binarySearch.java
@@ -5,6 +5,43 @@ public class binarySearch {
 
 
     public static void main(String[] args) {
-        
+
     }
 }
+
+
+
+/*
+ * https://www.geeksforgeeks.org/problems/binary-search-1587115620/1?page=1&difficulty=Basic&sortBy=submissions
+ * Given a sorted array of size N and an integer K, find the position(0-based indexing) at which K is present in the array using binary search.
+
+Example 1:
+Input:
+N = 5
+arr[] = {1 2 3 4 5} 
+K = 4
+Output: 3
+Explanation: 4 appears at index 3.
+
+Example 2:
+Input:
+N = 5
+arr[] = {11 22 33 44 55} 
+K = 445
+Output: -1
+Explanation: 445 is not present.
+
+Your Task:  
+You dont need to read input or print anything. Complete the function binarysearch() which takes arr[], N and K as input parameters and returns the index of K in the array. If K is not present in the array, return -1.
+
+
+Expected Time Complexity: O(LogN)
+Expected Auxiliary Space: O(LogN) if solving recursively and O(1) otherwise.
+
+
+Constraints:
+
+1 <= N <= 105
+1 <= arr[i] <= 106
+1 <= K <= 106
+ */
