25ea04f3fbdf82279be0a7eea6a6ce71cae14287
Anthea Ip <antheaip964@gmail.com>
2024-01-06 13:52:24 -0500

    Saving

--- binarySearch.java
+++ binarySearch.java
@@ -1,6 +1,14 @@
 public class binarySearch {
-    int static binarysearch(int arr[], int n, int k) {
+    static int binarysearch(int arr[], int n, int k) {
         // code here
+        // find midpoint of arr
+        // is midpoint == k; return index + 1
+        // if mid < k, search on right
+        int mid = int(Math.floor(n / 2));
+        while (mid != k) {
+
+        }
+        return 0;
     }
 
 
@@ -17,7 +25,8 @@ public class binarySearch {
 
 /*
  * https://www.geeksforgeeks.org/problems/binary-search-1587115620/1?page=1&difficulty=Basic&sortBy=submissions
- * Given a sorted array of size N and an integer K, find the position(0-based indexing) at which K is present in the array using binary search.
+ * Given a sorted array of size N and an integer K, find the position(0-based indexing) at which K is present in 
+ * the array using binary search.
 
 Example 1:
 Input:
@@ -36,7 +45,8 @@ Output: -1
 Explanation: 445 is not present.
 
 Your Task:  
-You dont need to read input or print anything. Complete the function binarysearch() which takes arr[], N and K as input parameters and returns the index of K in the array. If K is not present in the array, return -1.
+You dont need to read input or print anything. Complete the function binarysearch() which takes arr[], N and K 
+as input parameters and returns the index of K in the array. If K is not present in the array, return -1.
 
 
 Expected Time Complexity: O(LogN)
