11fa2e6965816024a18d13bc57f9970be2d143d8
Anthea Ip <antheaip964@gmail.com>
2024-01-10 14:49:36 -0500

    Saving

--- containerMostWater.java
+++ containerMostWater.java
@@ -6,27 +6,32 @@
  * 5. length = length of array - 1
  */
 
+ /*
+  * left = 0, right = length - 1 index
+  vol = (right-left) * Math.min(height[left], height[right]) 
+
+  */
 public class containerMostWater {
     public static int maxArea(int[] height) {
         
-        int maxArea = 0;
-        int left = 0;
-        int right = height.length - 1;
-        System.out.println("right: " + right);
-        System.out.println("heightright: " + height[right]);
-        while(left < right) {
-            int areaHeight = Math.min(height[left], height[right]);
-            int areaLen = right - left;
-            int area = areaHeight * areaLen;
-            System.out.println("area: " + area);
-            maxArea = Math.max(maxArea, area);
-            System.out.println("maxArea: " + maxArea);
-            // move the pointer of the min of the heights
-            if (height[left] <= height[right]) {
-                left++;
-            } else right--;
-        }
-        return maxArea;
+        // int maxArea = 0;
+        // int left = 0;
+        // int right = height.length - 1;
+        // System.out.println("right: " + right);
+        // System.out.println("heightright: " + height[right]);
+        // while(left < right) {
+        //     int areaHeight = Math.min(height[left], height[right]);
+        //     int areaLen = right - left;
+        //     int area = areaHeight * areaLen;
+        //     System.out.println("area: " + area);
+        //     maxArea = Math.max(maxArea, area);
+        //     System.out.println("maxArea: " + maxArea);
+        //     // move the pointer of the min of the heights
+        //     if (height[left] <= height[right]) {
+        //         left++;
+        //     } else right--;
+        // }
+        // return maxArea;
     }
 
 
