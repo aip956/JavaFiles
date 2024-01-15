599c5138ad2d5ef97be1e218047e374fe0ccf6e1
Anthea Ip <antheaip964@gmail.com>
2024-01-14 22:21:02 -0500

    Saving

--- OOP/Cat.java
+++ OOP/Cat.java
@@ -2,11 +2,15 @@
 Abstraction demonstrated by abstracting the common
 behaviors of animals into the 'Animal' class, creating
 specific subclasses like 'Dog' and 'Cat' that provide
-specialized implementations of the 'speak' method.
+specialized implementations of the 'speak' method (method
+override)
 
 Inheritance demonstrated by the relationship between
 'Animal' superclass and its subclasses 'Dog' and 'Cat',
 inheriting the 'name' attribute and the 'speak' method.
+
+Polymorphism demonstrated by speak having a different implementation
+than in the Animal class
 
 */
 
