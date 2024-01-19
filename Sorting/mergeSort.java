21904c9247b3cab81dfda1694ad9d3d1173fe090
Anthea Ip <antheaip964@gmail.com>
2024-01-18 20:25:44 -0500

    Saving

--- Sorting/mergeSort.java
+++ Sorting/mergeSort.java
@@ -5,8 +5,17 @@ public class mergeSort {
         // if array is empty or 1 element
         if (arr.length <= 1 || arr == null) { return; }
         // find mid index, copy left half and right half into leftArr and rightArr
+        // mergeSort leftArr and rightArr, then merge
+        int mid = arr.length / 2;
+        int[] leftArr = Arrays.copyOfRange(arr, 0, mid);
+        int[] rightArr = Arrays.copyOfRange(arr, mid+1, arr.length - 1);
 
-        
+
+        // merge: i, j, k; while i<left length, j<rightlength
+        // array[k++] = right or left
+        private static void merge(int[] left; int[] right; int[] sorted) {
+            int i = 0, j = 0, k = 0;
+        }
     }
 
 
