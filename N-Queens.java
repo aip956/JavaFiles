fda880ef2738e7f3aac135c046c52a3898512573
Anthea Ip <antheaip964@gmail.com>
2024-01-20 15:11:25 -0500

    Saving

---
+++ N-Queens.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class N-Queens {
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new List<>();
+
+        return result;
+    }
+
+
+    public static void main(String{} args) {
+        int n = 4;
+        System.out.pringln("Output: " + resutl);
+
+    }
+
+
+}
