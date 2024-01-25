22020ec82a733995a0ebd6cb28b9b5611383cc0f
Anthea Ip <antheaip964@gmail.com>
2024-01-24 22:30:53 -0500

    Saving

--- Sorting/insertionSort.java
+++ Sorting/insertionSort.java
@@ -2,13 +2,30 @@ import java.util.*;
 
 public class insertionSort {
     public static int[] insert(int[] arr) {
-        int[] sorted = new Array[arr.length];
-
-        return sorted;
+        for (int i = 1; i < arr.length; i++) {
+            System.out.println("i: " + i);
+            int j = i;
+            System.out.println("9j: " + j);
+            // while (j-1 >= 0) {
+            while (j - 1 >= 0 && arr[j] < arr[j - 1]) {
+                System.out.println("11j: " + j);
+                System.out.println("12arr[j]: " + arr[j]);
+                System.out.println("13arr[j-1]: " + arr[j-1]);
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp; 
+                }
+                j--;
+            }
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
-
+        int[] array = {6, 1, 10, 3, 5};
+        int[] result = insert(array);
+        System.out.println(Arrays.toString(result));
     }
 }
 
