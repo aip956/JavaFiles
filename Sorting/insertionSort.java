a7d3e16f97d34297dd7978feb793f802f6456357
Anthea Ip <antheaip964@gmail.com>
2024-01-24 22:08:44 -0500

    Saving

---
+++ Sorting/insertionSort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class insertionSort {
+    public static int[] insert(int[] arr) {
+        int[] sorted = new Array[arr.length];
+
+        return sorted;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
+ * Iterate through length of arr
+ * While i >= 0
+ *  If arr[i] < arr[i-1], swap
+ */
