815a6b80051ebeca7a4b571415d715e4403d0221
Anthea Ip <antheaip964@gmail.com>
2024-02-03 16:11:01 -0500

    Saving

--- dungeonGame.java
+++ dungeonGame.java
@@ -1,21 +1,25 @@
 public class dungeonGame {
-    public int calculateMinimumHP(int[][] dungeon) {
+    public static int calculateMinimumHP(int[][] dungeon) {
         int cols = dungeon[0].length;
         int rows = dungeon.length;
-        int maxCummul = 0;
+        int minCummul = 0;
+        
 
         for (int i = 0; i <= cols; i++) {
+            int sum = 0;
             for (int j = 0; j <= rows; j++) {
-                int sum
+                sum += dungeon[i][j];
+                minCummul = Math.min(minCummul, sum);
+
             }
         }
-        
+        return 0;
     }
 
 
     public static void main(String[] args) {
         int dungeon[][] = {{-2,-3,3},{-5,-10,1},{10,30,-5}};
-        System.out.println("output: ", calculateMinimumHP(dungeon));
+        System.out.println("output: " + calculateMinimumHP(dungeon));
     }
 
 
