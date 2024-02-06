58338e7dc5a50c646b69cbdea3cf2aaaa2783b82
Anthea Ip <antheaip964@gmail.com>
2024-02-06 10:41:21 -0500

    Saving

--- countInversion.java
+++ countInversion.java
@@ -1,8 +1,25 @@
 public class countInversion {
     static long inversionCount(long arr[], long N)
     {
-        // Your Code Here
-        long count = 0;
+        // Your Code Here; count inversions
+
+    }
+
+    public static void main(String[] args) {
+        // long[] arr = {2, 4, 1, 3, 5};
+        // long N = 5;
+        // long[] arr = {2, 3, 4, 5, 6};
+        // long N = 5;
+        long[] arr = {10, 10, 10};
+        long N = 3;
+        System.out.println(inversionCount(arr, N));
+    }
+}
+
+
+
+/* This solution works but is O(N^2) so times out; use merge sort instead
+ *         long count = 0;
         for (int i = 0; i < N - 1; i++) {
             for (int j = i + 1; j < N; j++) {
                 if (arr[i] > arr[j]) {
@@ -12,11 +29,4 @@ public class countInversion {
             }
         }
         return count;
-    }
-
-    public static void main(String[] args) {
-        long[] arr = {2, 4, 1, 3, 5};
-        long N = 5;
-        System.out.println(inversionCount(arr, N));
-    }
-}
+ */
