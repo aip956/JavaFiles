eee5ea7534b99e39e613e503747dd3d04d14b190
Anthea Ip <antheaip964@gmail.com>
2024-02-08 15:52:48 -0500

    Saving

---
+++ buddyStrings.java
@@ -0,0 +1,13 @@
+public class buddyStrings {
+    public boolean buddyStrings(String s, String goal) {
+        
+    }
+
+    public static void main(String[] args) {
+        String A = "aaaaaaabc";
+        String B = "aaaaaaacb";
+        System.out.println("Output: " + buddyStrings(A, B));
+    }
+    
+
+}
