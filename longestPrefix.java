c48495d8096358fca48ffdbdcaaa62cbb5622e1a
Anthea Ip <antheaip964@gmail.com>
2024-02-09 18:53:03 -0500

    Saving

--- longestPrefix.java
+++ longestPrefix.java
@@ -1,6 +1,16 @@
 public class longestPrefix {
     public static String longestCommonPrefix(String[] strs) {
-        
+        // iterate through the strings length - 1
+        // for each char in string, if in all elements, add char to string
+        String output = new String("");
+        for (int i = 0; i < strs.length(); i++) { // iterate through the array
+            for (int j = 0; j < strs[0].length(); j++) { // iterate in a word
+                char letter = strs[0][j]
+
+            }
+        }
+
+        return output;
     }
 
     public static void main(String[] args) {
