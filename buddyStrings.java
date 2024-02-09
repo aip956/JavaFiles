d5265437b5008090d8065c8e7cefe39b18625bbc
Anthea Ip <antheaip964@gmail.com>
2024-02-08 19:39:52 -0500

    Saving

--- buddyStrings.java
+++ buddyStrings.java
@@ -1,14 +1,33 @@
 import java.util.*;
 public class buddyStrings {
     public boolean buddyStrings(String s, String goal) {
-        // test they are the same length
-        // if A==B, if there are repeated chars, return true
-        // else count number of differences; if there are only 2 AND the strings are equal after swap, return tru
+        // 1. test they are the same length
+        // 2. if A==B, if there are repeated chars, return true
+        // 3. else count number of differences; if there are only 2 AND the strings are equal after swap, return tru
+        // 1.
         if (s.length() != goal.length()) return false;
-        if (s.equals(goal)) {
-            
-        }
 
+        // 2.
+        if (s.equals(goal)) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.lastIndexOf(s.charAt(i)) != i) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        // 3. 
+        int diffCount = 0;
+        int[] diffIndicies = new int[2];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                diffIndicies[diffCount] = i;
+                diffCount++;
+            }
+        }
+        if (diffCount != 2) return false;
+        if ()
         
     }
 
