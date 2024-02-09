3db4b72cfa4822ce210b88743bc2ef649c51c5d0
Anthea Ip <antheaip964@gmail.com>
2024-02-09 18:39:47 -0500

    Saving

---
+++ longestPrefix.java
@@ -0,0 +1,11 @@
+public class longestPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        
+    }
+
+    public static void main(String[] args) {
+        String[] strs1 = {"flower","flow","flight"};
+        String[] strs2 = {"dog","racecar","car"};
+        System.out.println("Output1: " + longestCommonPrefix(strs1)); // "fl"
+        System.out.println("Output2: " + longestCommonPrefix(strs2)); // ""
+}
