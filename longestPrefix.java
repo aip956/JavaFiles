bde3fd1c3849d946b28befe3e04528694cc2c908
Anthea Ip <antheaip964@gmail.com>
2024-02-12 14:53:17 -0500

    Saving

--- longestPrefix.java
+++ longestPrefix.java
@@ -2,16 +2,19 @@ public class longestPrefix {
     public static String longestCommonPrefix(String[] strs) {
         // iterate through the strings length - 1
         // for each char in string, if in all elements, add char to string
-        String output = new String("");
-        for (int i = 0; i < strs.length(); i++) { // iterate through the array
-            for (int j = 0; j < strs[0].length(); j++) { // iterate in a word
-                char letter = strs[0][j];
-                if (charAt(strs[i][j])equals(letter)
-
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) { // iterate in a word
+            char currLetter = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) { // iterate through the array words
+                // if i is >= word or not= letter, stop and return
+                if (i >= strs[j].length() || strs[j].charAt(i) != currLetter){
+                    return output.toString();
+                }
             }
+            output.append(currLetter);
         }
-
-        return output;
+        return output.toString();
+        
     }
 
     public static void main(String[] args) {
@@ -19,4 +22,5 @@ public class longestPrefix {
         String[] strs2 = {"dog","racecar","car"};
         System.out.println("Output1: " + longestCommonPrefix(strs1)); // "fl"
         System.out.println("Output2: " + longestCommonPrefix(strs2)); // ""
+    }
 }
