c911e26220b506882b66b2dcb9afcec30f149772
Anthea Ip <antheaip964@gmail.com>
2024-02-12 15:15:29 -0500

    Saving

--- longestPrefix.java
+++ longestPrefix.java
@@ -14,7 +14,6 @@ public class longestPrefix {
             output.append(currLetter);
         }
         return output.toString();
-        
     }
 
     public static void main(String[] args) {
@@ -24,3 +23,38 @@ public class longestPrefix {
         System.out.println("Output2: " + longestCommonPrefix(strs2)); // ""
     }
 }
+
+/*
+Space: O(m)
+    Stringbuilter for output, length of common prefix can be equal
+    to the shortest string(m). Also use constant amount of space for currLetter,
+    i, j
+Time: O(n*m) 
+    Outer loop iterates throughe ach char position in the first string, length m
+    Inner loop iterates through each of the other n-1 strings in array. 
+    Worst case, iterates through entire length of each string, m (O(n-1)*m)
+    Combined: O(m+(n-1)*m) = O(n*m)
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ * Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+ */
