baca6127137e7de66e00af02ba9c2cd5b21036e1
Anthea Ip <antheaip964@gmail.com>
2024-02-14 19:04:51 -0500

    Saving

---
+++ Neetcode/dynArr.java
@@ -0,0 +1,35 @@
+
+class DynamicArray {
+
+    public DynamicArray(int capacity) {
+
+    }
+
+    public int get(int i) {
+
+    }
+
+    public void set(int i, int n) {
+
+    }
+
+    public void pushback(int n) {
+
+    }
+
+    public int popback() {
+
+    }
+
+    private void resize() {
+
+    }
+
+    public int getSize() {
+
+    }
+
+    public int getCapacity() {
+
+    }
+}
