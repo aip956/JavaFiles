b874d3ceccc2f53194b18392c93ac546cfa3e7ac
Anthea Ip <antheaip964@gmail.com>
2024-02-15 12:49:25 -0500

    Saving

---
+++ romanToInt.java
@@ -0,0 +1,21 @@
+public class romanToInt {
+    public static int romanToInt(String s) {
+        /* 
+        convert to charArray
+        create hash of roman:value
+        create int total, prev
+        iterate through string
+        for letter in string, add value from has to total
+        if value > prev value, total -= 2*prev
+        */
+    }
+
+    public static void main(String[] args) {
+        String s1 = "III";
+        System.out.println("Output1: " + romanToInt(s1));
+        String s2 = "LVIII";
+        System.out.println("Output2: " + romanToInt(s2));
+        String s3 = "MCMXCIV";
+        System.out.println("Output3: " + romanToInt(s3));
+    }
+}
