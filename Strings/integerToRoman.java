44e0318d9e42038222ab3b059fc06ef90798ce04
Anthea Ip <antheaip964@gmail.com>
2024-02-15 19:06:04 -0500

    Saving

---
+++ Strings/integerToRoman.java
@@ -0,0 +1,19 @@
+public class integerToRoman {
+    public static String intToRoman(int num) {
+        /*
+         * create hash for letter:num; IV, V, IX, X, 
+         * 
+         * string output, int prev
+         * if 
+         */
+    }
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 58;
+        int num3 = 1994;
+        System.out.println("Output1: " + intToRoman(num1));
+        System.out.println("Output2: " + intToRoman(num2));
+        System.out.println("Output3: " + intToRoman(num3));
+    }
+}
