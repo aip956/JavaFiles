c6e49e81850696241379696b9e62f795c832286a
Anthea Ip <antheaip964@gmail.com>
2024-02-19 16:31:00 -0500

    Saving

---
+++ degreeOfArray.java
@@ -0,0 +1,14 @@
+public class degreeOfArray {
+    public int findShortestSubArray(int[] nums) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,2,3,1,4,2};
+        int[] arr2 = {1, 2, 2, 3, 1};
+        System.out.println(degOfArr(arr1));
+        System.out.println(degOfArr(arr2));
+    
+      }
+
+}
