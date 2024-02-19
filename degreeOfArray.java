f9916c3a29c37f85e58134cb01f7a2ece169ff4b
Anthea Ip <antheaip964@gmail.com>
2024-02-19 18:24:39 -0500

    Saving

--- degreeOfArray.java
+++ degreeOfArray.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class degreeOfArray {
     public static int findShortestSubArray(int[] nums) {
         HashMap <Integer, Integer> freqMap = new HashMap<>();
-        int short_arr_len = 0;
+        int short_arr_len = Integer.MAX_VALUE;
         for (int num : nums) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) +1);
         }
@@ -13,11 +13,15 @@ public class degreeOfArray {
         for (int freq : freqMap.values()) {
             degree = Math.max(degree, freq);
         }
+        System.out.println("16degree: " + degree);
 
         // Find first and last occurence indices of elements with degree freq
         
         for (int num : freqMap.keySet()) { // iterate through the values
+            System.out.println("21num: " + num);
+            System.out.println("22freqMap.keySet(): " + freqMap.keySet());
             if(freqMap.get(num) == degree) { // if key num's value = degree
+            
             int first_ind = -1;
             int last_ind = -1;
 
@@ -29,7 +33,7 @@ public class degreeOfArray {
                     last_ind = i;
                 }
             }
-            short_arr_len = Math.min(short_arr_len, (last_ind = first_ind + 1));
+            short_arr_len = Math.min(short_arr_len, (last_ind - first_ind + 1));
             }
         }
         return short_arr_len;
@@ -38,13 +42,14 @@ public class degreeOfArray {
     public static void main(String[] args) {
         int[] arr1 = {1,2,2,3,1,4,2};
         int[] arr2 = {1, 2, 2, 3, 1};
-        System.out.println(findShortestSubArray(arr1));
-        System.out.println(findShortestSubArray(arr2));
+        System.out.println("Output1: " + findShortestSubArray(arr1));
+        System.out.println("Output2: " + findShortestSubArray(arr2));
     
       }
 
 }
 /*
+https://leetcode.com/problems/degree-of-an-array/submissions/1180371928/
 Make frequency map
 Find degree; greatest vals in freq map
  * Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
