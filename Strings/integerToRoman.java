2c5a28ceed1ee31a9cb558d644e11101ae52210d
Anthea Ip <antheaip964@gmail.com>
2024-02-20 11:02:55 -0500

    Saving

--- Strings/integerToRoman.java
+++ Strings/integerToRoman.java
@@ -1,6 +1,53 @@
+import java.util.HashMap;
+
 public class integerToRoman {
     public static String intToRoman(int num) {
-        /*
+        HashMap<String, Integer> roMap = new HashMap<>();
+        roMap.put("M", 1000);
+        roMap.put("CM", 900);
+        roMap.put( "D", 500);
+        roMap.put("CD", 400);
+        roMap.put("C", 100);
+        roMap.put("XC", 90);
+        roMap.put("L", 50);
+        roMap.put("XL", 40);
+        roMap.put("X", 10);
+        roMap.put("IX", 9);
+        roMap.put("V", 5);
+        roMap.put("IV", 4);
+        roMap.put("I", 1);
+        // create list of sorted romans
+        List<String> sortedRomans = new 
+        StringBuilder result = new StringBuilder();
+        while (num > 0) {
+            for (String roman : roMap.keySet()) {
+                // System.out.println("8roman: " + roman);
+                // System.out.println("9roMap.values(): " + roMap.values());
+
+                if (num >= roMap.get(roman)) {
+                    result.append(roman);
+                    System.out.println("10result: " + result);
+                    num -= roMap.get(roman);
+                    System.out.println("15num: " + num);
+                    break;
+                }
+            }
+        }
+        return result.toString();    
+    }
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 58;
+        int num3 = 1994;
+        System.out.println("Output1: " + intToRoman(num1));
+        System.out.println("Output2: " + intToRoman(num2));
+        System.out.println("Output3: " + intToRoman(num3));
+    }
+}
+
+/*
+/*
         if input num > highest roman, add it to the string builder and 
         reduce corresponding val from input
         if input < highest roman, then check with next highest roman
@@ -26,20 +73,6 @@ public class integerToRoman {
         2 > 1, add I, num - 1 = 1, string = LVII
         1 >= 1, add I, num - 1 = 0, string = LVIII
 
-         */
-    }
-
-    public static void main(String[] args) {
-        int num1 = 3;
-        int num2 = 58;
-        int num3 = 1994;
-        System.out.println("Output1: " + intToRoman(num1));
-        System.out.println("Output2: " + intToRoman(num2));
-        System.out.println("Output3: " + intToRoman(num3));
-    }
-}
-
-/*
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
 Symbol       Value
