d630b15e3b859779d1814ff77581723a2551e91d
Anthea Ip <antheaip964@gmail.com>
2024-02-24 13:22:53 -0500

    Saving

---
+++ setMatrixZeros.java
@@ -0,0 +1,17 @@
+public class setMatrixZeros {
+    public static void setZeroes(int[][] matrix) {
+        Syste
+        
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1,1,1},{1,0,1},{1,1,1}};
+        System.out.println("Output1: " + setZeroes(matrix1));
+    }
+}
+
+
+/*
+ * https://leetcode.com/problems/set-matrix-zeroes/description/
+ * 
+ */
