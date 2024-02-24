225f219a8f5d4e9676a9988654dbe8f6e4db1b8a
Anthea Ip <antheaip964@gmail.com>
2024-02-24 13:34:27 -0500

    Saving

--- setMatrixZeros.java
+++ setMatrixZeros.java
@@ -1,6 +1,40 @@
+import java.util.*;
 public class setMatrixZeros {
     public static void setZeroes(int[][] matrix) {
-        Syste
+        System.out.println("3Mtx: " + matrix);
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int Col = 1;
+
+        // Travers matrix and mark 1st row and 1st col as follows:
+        for (int i = 0; i < n; i++) { // for each row
+            for (int j = 0; j < m; j++) { // each column
+                if (matrix[i][j] == 0) {
+                    // mark ith row
+                    matrix[i][0] = 0; // will mark the column of the ith row as 0
+                    // mark the jth col
+                    if (j == 0) Col = 0;
+                    else matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Mark with 0 from (1,1) to (n-1, m-1):
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    // Check for col & row
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // mark the 1st row then 1st col
+        if (matrix[0][0] == 0) {
+            Arrays.fill(matrix[0], 0);
+        }
         
     }
 
