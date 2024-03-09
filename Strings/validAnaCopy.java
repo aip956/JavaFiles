3092fe94f300aa6f3b94d6fa6aca47d424fb46e4
Anthea Ip <antheaip964@gmail.com>
2024-03-09 11:38:27 -0500

    Saving

---
+++ Strings/validAnaCopy.java
@@ -0,0 +1,14 @@
+public class validAnaCopy {
+    public static boolean isAnagram(String s, String t) {
+
+
+
+    }
+    public static void main(String[] args) {
+        String s1 = "anagram";
+        String t1 = "nagaram";
+        System.out.println("Output1: " + isAnagram(s1, t1));
+    }
+
+
+}
