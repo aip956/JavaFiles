579e7289e6d6a5ea0fc719d86fe21b688c15357b
Anthea Ip <antheaip964@gmail.com>
2024-03-09 12:24:40 -0500

    Saving

--- Strings/validAnaCopy.java
+++ Strings/validAnaCopy.java
@@ -1,8 +1,8 @@
+import java.util.HashMap;
+
 public class validAnaCopy {
     public static boolean isAnagram(String s, String t) {
-
-
-
+       
     }
     public static void main(String[] args) {
         String s1 = "anagram";
@@ -12,3 +12,26 @@ public class validAnaCopy {
 
 
 }
+/*
+Time: O(n); n is length of strings s and t
+Space:
+if not equal length, return false
+
+create hash of letter, frequency, for string s
+if shash does not have letter in t return false
+if count <= 0 return false
+for string t, remove elements from hash
+
+return if hash is empty
+ * https://leetcode.com/problems/valid-anagram/
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+ */
