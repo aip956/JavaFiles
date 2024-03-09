f1d9a90d270dbc5b084b245f4da482697a81b9e2
Anthea Ip <antheaip964@gmail.com>
2024-03-09 13:57:46 -0500

    Saving

---
+++ Strings/groupAnag.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class groupAnag {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        
+    }
+
+    public static void main(String[] args) {
+        String[] strs1 = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println("Output1: " + groupAnagrams(strs1));
+
+    }
+}
+/*
+ * https://leetcode.com/problems/group-anagrams/description/
+ * Given an array of strings strs, group the anagrams together. 
+ * You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters 
+of a different word or phrase, typically using all the original 
+letters exactly once.
+
+ 
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+
+ */
