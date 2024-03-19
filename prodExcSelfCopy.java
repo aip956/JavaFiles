855e251d35fdf8eee935c7132ad4b51ad7671cab
Anthea Ip <antheaip964@gmail.com>
2024-03-19 18:07:02 -0400

    Saving

---
+++ prodExcSelfCopy.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class prodExcSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        
+
+        
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4};
+        System.out.println("Output: " + Arrays.toString(productExceptSelf(nums1)));
+        // int[] nums2 = {-1,1,0,-3,3};
+        // System.out.println("Output: " + Arrays.toString(productExceptSelf(nums2)));
+    }
+}
