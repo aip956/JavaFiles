14eedf7ff0d59876a4ce41d676245c3cf5ee5169
Anthea Ip <antheaip964@gmail.com>
2024-03-22 12:26:07 -0400

    Saving

---
+++ firstUn2.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class firstUn2 {
+    public static int firstUniqChar(String s) {
+        
+    }
+
+    public static void main(String[] args) {
+        String s1 = "leetcode";
+        System.out.println("Output1: " + firstUniqChar(s1));
+        // String s2 = "loveleetcode";
+        // System.out.println("Output2: " + firstUniqChar(s2));
+        // String s3 = "aabb";
+        // System.out.println("Output3: " + firstUniqChar(s3));
+    }
+}
