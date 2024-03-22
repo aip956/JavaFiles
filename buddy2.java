f435a988e73d1c421ba1b8a85ae82b016ddb0bea
Anthea Ip <antheaip964@gmail.com>
2024-03-21 21:11:43 -0400

    Saving

---
+++ buddy2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class buddy2 {
+    public static boolean buddyStrings(String s, String goal) {
+        // if they are not the same length, return false
+        // if they are the same strings, if there are repeated chars; yes-true
+        //  same len, diff strings; if there are 2 unique chars,
+            
+      
+    }
+
+    public static void main(String[] args) {
+        String s = "aaaaaaabc";
+        String goal = "aaaaaaacb";
+        System.out.println("Output: " + buddyStrings(s, goal));
+    }
+
+}
+        // 1. test they are the same length
+        // 2. if A==B, if there are repeated chars, return true
+        // 3. else count number of differences; if there are only 2 
+        // AND the strings are equal after swap, return tru
+        // 1.
+       
+        // 2.
+        
+        
+        // 3. 
