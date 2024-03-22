60de81f6370797eb51f5d4a67397fd65c70c1bdc
Anthea Ip <antheaip964@gmail.com>
2024-03-22 12:35:22 -0400

    Saving

--- firstUn2.java
+++ firstUn2.java
@@ -1,8 +1,21 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class firstUn2 {
     public static int firstUniqChar(String s) {
-        
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        char[] s_arr = s.toCharArray();
+        for (Character c : s_arr) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+        System.out.println("10: freqM: " + freqMap);
+
+        // iterate through s_arr for freq of 1
+        for (int i = 0; i < s_arr.length; i++) {
+            if (freqMap.get(s_arr[i]) == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -14,3 +27,10 @@ public class firstUn2 {
         // System.out.println("Output3: " + firstUniqChar(s3));
     }
 }
+
+/*
+ *  * make a hashmap of char and freq
+ * return first char of freq 1, or -1
+ * Given a string s, find the first non-repeating character in it and 
+ * return its index. If it does not exist, return -1.
+ */
