9e51270717dab45ad17f40da4660c0a63cdbd943
Anthea Ip <antheaip964@gmail.com>
2024-03-28 18:40:37 -0400

    Saving

--- numberRecentCalls.java
+++ numberRecentCalls.java
@@ -1,9 +1,9 @@
 import java.util.*;
-Queue<Integer> queue;
-class RecentCounter {
 
+class RecentCounter {
+    Queue<Integer> queue;
     public RecentCounter() {
-        queue = new LinkedList();
+        queue = new LinkedList<>();
     }
     
     public int ping(int t) {
@@ -15,6 +15,24 @@ class RecentCounter {
     }
 
     public static void main(String[] args) {
+        RecentCounter recentCounter = new RecentCounter();
+        String[] commands = {"RecentCounter", "ping", "ping", "ping", "ping"};
+        int[][] arguments = {{}, {1}, {100}, {3001}, {3002}};
+        for (int i = 1; i < commands.length; i++) {
+            String command = commands[i];
+            int[] argument = arguments[i];
 
+            if (command.equals("ping")) {
+                int pingResult = recentCounter.ping(argument[0]);
+                System.out.println("ping(" + argument[0] + ") = " + pingResult);
+            } else {
+                System.err.println("Invalid command: " + command);
+            }
+        }
     }
 }
+
+/*
+ * to compile: javac numberRecentCalls.java
+ * to run: java RecentCounter
+ */
