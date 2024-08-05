646f32a46e8cbfba367f9afc19a4742d2f9f0608
Anthea Ip <antheaip964@gmail.com>
2024-08-04 20:54:16 -0400

    Practice

--- test.java
+++ test.java
@@ -1,34 +1,31 @@
 import java.util.*;
 
 public class test {
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        // for each word, 
-        // sort it
-        // if sorted word is not in map, put it
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String word : strs) {
-            char[] wordArr = word.toCharArray();
-            Arrays.sort(wordArr);
-            String sortedWord = new String(wordArr);
-            if (!map.containsKey (sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
-                System.out.println("15map: " + map);
-            }
-        // Add word to the sortedWords's list
-        map.get(sortedWord).add(word);
-        System.out.println("20map: " + map);
+    public static boolean isAnagram(String s, String t) {
+        // Create a frequency hash
+        // add string s to hash
+        // for string t, subtract each letter
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (Character letter : s.toCharArray()) {
+            freq.put(letter, freq.getOrDefault(letter, 0) + 1);
         }
-        // return the values of the map without key
-        return new ArrayList<>(map.values()); 
+        for (Character letter : t.toCharArray()) {
+            if (!freq.containsKey(letter)) return false;
+            int count = freq.get(letter);
+            if (count <= 0) return false;
+            freq.put(letter, count-1);
+        }
+        return true;
     }
 
 
 
 
     public static void main(String[] args) {
-        String[] str1 = {"eat","tea","tan","ate","nat","bat"};
-        System.out.println("Output1: " + groupAnagrams(str1));
-        String[] str2 = {""};
-        System.out.println("Output2: " + groupAnagrams(str2));
+        String s1 = "rat";
+        String t1 = "car";
+        System.out.println("Output1: " + isAnagram(s1, t1));
+
     }
 }
--- topKFreqElem.java
+++ topKFreqElem.java
@@ -45,3 +45,18 @@ public class topKFreqElem {
         System.out.println("Output: " + Arrays.toString((topKFrequent(nums, k))));
     }
 }
+
+/*
+ * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ */
