812ed91ceca4901942ec88ea8fa75b171f4ed617
Anthea Ip <antheaip964@gmail.com>
2024-08-04 21:12:41 -0400

    Practice

--- test.java
+++ test.java
@@ -1,31 +1,23 @@
 import java.util.*;
 
 public class test {
-    public static boolean isAnagram(String s, String t) {
-        // Create a frequency hash
-        // add string s to hash
-        // for string t, subtract each letter
-        if (s.length() != t.length()) return false;
-        HashMap<Character, Integer> freq = new HashMap<>();
-        for (Character letter : s.toCharArray()) {
-            freq.put(letter, freq.getOrDefault(letter, 0) + 1);
-        }
-        for (Character letter : t.toCharArray()) {
-            if (!freq.containsKey(letter)) return false;
-            int count = freq.get(letter);
-            if (count <= 0) return false;
-            freq.put(letter, count-1);
-        }
-        return true;
+    public static int[] topKFrequent(int[] nums, int k) {
+        // create a hashmap to store the frequency of each number
+        // create a priority queue to store the numbers in order of frequency
+        
+
+
+
     }
 
 
 
 
     public static void main(String[] args) {
-        String s1 = "rat";
-        String t1 = "car";
-        System.out.println("Output1: " + isAnagram(s1, t1));
+        int nums1 = {1,1,1,2,2,3};
+        int k1 = 2;
+
+        System.out.println("Output1: " + topKFrequent(nums1, k1));
 
     }
 }
