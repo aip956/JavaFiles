93d6a16532068d2cdb99aa532e9e604ed80fd266
Anthea Ip <antheaip964@gmail.com>
2024-08-10 16:24:13 -0400

    Practice

--- rotateImage.java
+++ rotateImage.java
@@ -2,27 +2,46 @@ import java.io.*;
 
 class Solution {
     public static void rotate(int[][] matrix) {
-        int n = length(matrix);
+        int n = matrix.length;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 // swap the elements
+                System.out.println("matrix[i][j] " + matrix[i][j]);
+                System.out.println("matrix[j][i]" + matrix[j][i]);
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
-            System.out.println("13Matrix: " + matrix);
+            System.out.println("13Matrix:");
+            printMatrix(matrix);
         }
 
         // reverse the rows
         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j-1];
+                matrix[i][n - j - 1] = temp;
+            }
+            System.out.println("23Matrix:");
+            printMatrix(matrix);
             
         }
+        System.out.println("25Matrix:");
+        printMatrix(matrix);
     }
 
-
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
-        System.out.println(rotate(matrix));
+        rotate(matrix);
     }
 }
 
