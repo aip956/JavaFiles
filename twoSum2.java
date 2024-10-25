b64cdccfa8d22abb89828b6f7e4dc57a1039dc54
Anthea Ip <antheaip964@gmail.com>
2024-10-24 21:04:09 -0400

    Java

--- twoSum2.java
+++ twoSum2.java
@@ -1,13 +1,26 @@
 import java.util.*
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Given an array of integers nums and an integer 
+target, return indices of the two numbers such 
+that they add up to target.
+You may assume that each input would have exactly
+ one solution, and you may not use the same 
+ element twice.
 
 You can return the answer in any order.
+
+Sort the array
+While 
+Use left and right pointers
+If the sum is == target, return left, right
+If the sum is < target, move left pointer +1
+If the sum is > target, move right pointer -1
  */
 public class twoSum2 {
-    #
+    left_ind = 0;
+    right_ind = nums.length - 1;
+    Arrays.sort(nums);
+    
     
 
 }
