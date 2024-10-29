482ded0797a529602fde34cf77452370c57cc2d0
Anthea Ip <antheaip964@gmail.com>
2024-10-28 22:08:59 -0400

    Java

---
+++ Solution.java
@@ -0,0 +1,31 @@
+/*
+ * Design an algorithm to encode a list of strings
+ *  to a string. The encoded string is then sent over
+ *  the network and is decoded back to the original 
+ * list of strings.
+
+Please implement encode and decode
+ */
+
+ public class Solution {
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    public String encode(List<String> strs) {
+        // write your code here
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+    public List<String> decode(String str) {
+        // write your code here
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
