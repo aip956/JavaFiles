9118cc870cbdab5df30e091d9b49b5bfcd225c66
Anthea Ip <antheaip964@gmail.com>
2024-10-28 21:04:10 -0400

    Java

--- TopKFrequentWord.java
+++ TopKFrequentWord.java
@@ -4,20 +4,47 @@
          * If words have same freq, order lexicographically
          * Extract k words
          */
-
 import java.util.*;
-        class Solution {
-            public List<String> topKFrequent(String[] words, int k) {
-                HashMap<String, Integer> freq = new HashMap<>();
-                for (String word:)
-                Queue<String, Integer> queue = new PriorityQueue<>();
-                
+public class TopKFrequentWord {
+    public static List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> freq = new HashMap<>();
+        for (String word : words) {
+            freq.put(word, freq.getOrDefault(word, 0) + 1);
+        }
+        // Create priority queue by freq, and lex
+        PriorityQueue<Map.Entry<String, Integer>> queue = new PriorityQueue<>((a, b) -> {
+            // Sort by freq
+            if (!a.getValue().equals(b.getValue())) {
+                return a.getValue() - b.getValue();
             }
-
-
-            public static void main(String[] args) {
-                String[] words = {"i","love","leetcode","i","love","coding"};
-                int k = 2;
-                System.out.println(topKFreqElem(words, k));
+            // Sort by lex
+            return b.getKey().compareTo(a.getKey());
+            }
+        );
+    
+        // Add each entry from the freq map to the queue
+        for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+            queue.offer(entry);
+            if (queue.size() > k) {
+                queue.poll();
             }
         }
+        // Extract elements from the queue
+        List<String> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            result.add(queue.poll().getKey());
+        }
+
+        // Reverse the list
+        Collections.reverse(result);
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+        String[] words = {"i","love","leetcode","i","love","coding"};
+        int k = 2;
+        System.out.println(topKFrequent(words, k));
+    }
+}
--- topKFreqElem.java
+++ topKFreqElem.java
@@ -4,39 +4,54 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+/*
+ * Create a hashmap to store the frequency of each number
+ * Create a priority queue to store the numbers in order of frequency
+ * For each number in the input array, add to the hashmap
+ * For each number in the hashmap, add to the priority queue
+ * If the size of the priority queue is greater than k, remove the least frequent element
+ * Add the remaining elements to the result array
+ * Return the result array
+ */
 
 public class topKFreqElem {
     public static int[] topKFrequent(int[] nums, int k) {
-       
+       HashMap<Integer, Integer> freqMap = new HashMap<>();
+       for (int num : nums) {
+        freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+       }
+       System.out.println("FreqMap: ", freqMap);
+
+       PriorityQueue<Integer> pq = new PriorityQueue<>()
 
 
 
 
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-        // add to Hash the number, freq
-        for (int number : nums) {
-            freqMap.put(number, freqMap.getOrDefault(number, 0) + 1);
-            System.out.println("freqMap: " + freqMap);
-        }
-        // create a pq prioritizing the value/freq of freqMap
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(freqMap::get));
+        // HashMap<Integer, Integer> freqMap = new HashMap<>();
+        // // add to Hash the number, freq
+        // for (int number : nums) {
+        //     freqMap.put(number, freqMap.getOrDefault(number, 0) + 1);
+        //     System.out.println("freqMap: " + freqMap);
+        // }
+        // // create a pq prioritizing the value/freq of freqMap
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(freqMap::get));
 
-        for (int num : freqMap.keySet()) {
-            pq.offer(num); // add the num to the pq
-            System.out.println("PQ: " + pq);
-            if (pq.size() > k) {
-                pq.poll(); // remove least freq elements if q size > k
-            }
-        }
+        // for (int num : freqMap.keySet()) {
+        //     pq.offer(num); // add the num to the pq
+        //     System.out.println("PQ: " + pq);
+        //     if (pq.size() > k) {
+        //         pq.poll(); // remove least freq elements if q size > k
+        //     }
+        // }
 
-        int[] result = new int[k]; // size limited to k
-        int index = 0;
-        while (!pq.isEmpty()) {
-            System.out.println("index: " + index);
-            result[index++] = pq.poll();
-            System.out.println("result: " + Arrays.toString(result));
-        }
-        return result;
+        // int[] result = new int[k]; // size limited to k
+        // int index = 0;
+        // while (!pq.isEmpty()) {
+        //     System.out.println("index: " + index);
+        //     result[index++] = pq.poll();
+        //     System.out.println("result: " + Arrays.toString(result));
+        // }
+        // return result;
     }
 
 
