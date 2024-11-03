9b7039c7d1f03e8821bf03d29ecc59777a7ea3ab
Anthea Ip <antheaip964@gmail.com>
2024-11-02 21:46:59 -0400

    Java

--- validPali2.java
+++ validPali2.java
@@ -1,13 +1,14 @@
 import java.util.*;
 class validPali2 {
-    public boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
         // create a string sb with only letters and nums
         // two pointers, l and r
         // while l<r
         // char at sb[r] must != sb[l] return false
         StringBuilder sb = new StringBuilder();
         // convert s to lower case
-        s.toLowerCase();
+        s = s.toLowerCase();
+        System.out.println("11s: " + s);
         // create sb = s with just letters and nums
         for (char letter : s.toCharArray()) {
             if (Character.isLetterOrDigit(letter)) {
@@ -16,18 +17,22 @@ class validPali2 {
             System.out.println("15sb: " + sb);
         }
         // iterate from both ends
-        int right = sb.length();
+        int right = sb.length() - 1;
         int left = 0;
         while(left < right) {
-            
+            if (sb.charAt(left) != sb.charAt(right)) {
+                System.out.println(sb.charAt(left));
+                System.out.println(sb.charAt(right));
+                return false;
+            }
+            left++;
+            right--;
         }
-      
-        
     return true;
     }
 
     public static void main(String[] args) {
-        input1 = "A man, a plan, a canal: Panama";
-        print(isPalindrome(input1));
+        String input1 = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(input1));
     }
 }
