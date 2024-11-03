4a7d219471d86afb9cb43c7c7ed730e99a41f161
Anthea Ip <antheaip964@gmail.com>
2024-11-03 12:41:21 -0500

    Java

---
+++ containerWater.java
@@ -0,0 +1,11 @@
+
+class containerWater {
+    public static int maxArea(int[] height) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+}
