eed2d18c63fe88f0b806cbf8b53819fc4346a55b
Anthea Ip <antheaip964@gmail.com>
2024-11-03 20:24:15 -0500

    Java

---
+++ slidingWindow/buySellStock.java
@@ -0,0 +1,15 @@
+import java.util.*/**
+ * buySellStock
+ */
+public class buySellStock {
+    public int maxProfit(int[] prices) {
+        
+    }
+
+    public static void main(String[] args) {
+        int prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices)); // 5
+    }
+    
+
+}
