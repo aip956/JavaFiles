735ab80d3a234a1d06d1b3b73cf4d70ca6c8bb63
Anthea Ip <antheaip964@gmail.com>
2024-11-07 19:33:31 -0500

    Java

---
+++ Stack/ValidParen.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ValidParen {
+    public static boolean isValid(String s) {
+        
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        System.out.println(isValid(s));
+    }
+    
+}
