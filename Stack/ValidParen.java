2987797174ea89fc32959023576ad71b151b5185
Anthea Ip <antheaip964@gmail.com>
2024-11-07 19:46:01 -0500

    Java

--- Stack/ValidParen.java
+++ Stack/ValidParen.java
@@ -2,9 +2,26 @@ import java.util.*;
 
 public class ValidParen {
     public static boolean isValid(String s) {
-        
+        Stack<Character> stack = new Stack<>();
 
-        return false;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+            
+            char top = stack.pop();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
