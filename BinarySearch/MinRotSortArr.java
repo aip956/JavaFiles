b219f42a2cd2b4087492b6d321c302622effc361
Anthea Ip <antheaip964@gmail.com>
2024-11-10 16:49:50 -0500

    Java

--- BinarySearch/MinRotSortArr.java
+++ BinarySearch/MinRotSortArr.java
@@ -1,7 +1,12 @@
 import java.util.*;
 
 class MinRotSortArr {
-    public int findMin(int[] nums) {
+    public static int findMin(int[] nums) {
         
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(findMin(nums));
     }
 }
