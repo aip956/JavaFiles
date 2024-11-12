25c4ce75942ad5f820fb2b235f414ae03e539a6c
Anthea Ip <antheaip964@gmail.com>
2024-11-11 20:18:59 -0500

    Java

--- BinarySearch/MinRotSortArr.java
+++ BinarySearch/MinRotSortArr.java
@@ -6,22 +6,26 @@ class MinRotSortArr {
         int right = nums.length - 1;
 
         while (left < right) {
+            System.out.println("9l: " + nums[left]);
+            System.out.println("10r: " + nums[right]);
             int mid = (right + left) / 2;
-
+            System.out.println("12mid: " + nums[mid]);
             // Check if mid is in the rotated part or sorted part
             if (nums[mid] > nums[right]) {
                 // Min must be in right half
                 left = mid + 1;
+                System.out.println("17L: " + nums[left]);
             } else {
                 // Min could be mid or in the left half
                 right = mid;
+                System.out.println("21R: " + nums[right]);
             }
         }
         return nums[left];
     }
 
     public static void main(String[] args) {
-        int[] nums = {3,4,5,1,2};
+        int[] nums = {4,5,1,2,3};
         System.out.println(findMin(nums));
     }
 }
@@ -31,7 +35,9 @@ class MinRotSortArr {
  1. Binary Search Loop
  Narrow range [left, right] until left = right
  2. Condition Check
- nums[mid] > nums[right]: rotation on right side so move left to mid + 1
+ [3, 4, 5, 1, 2]
+ [4, 5, 1, 2, 3] => [4, 5, 1]
+ nums[mid] > nums[right]: rotation on right side so move left to mid + 1; search the right side
  nums[mid] <= nums[right]: min is on left side or is nums[mid], so move right to mid
  3. Return result
  When left == right, min elem is at nums[left]
