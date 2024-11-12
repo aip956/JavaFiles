f3caa80811d2662c0333d180c4254070c9a7f8ba
Anthea Ip <antheaip964@gmail.com>
2024-11-11 21:12:10 -0500

    Java

--- LinkedList/RevLinkList.java
+++ LinkedList/RevLinkList.java
@@ -9,3 +9,23 @@ class RevLinkList {
         
     }
 }
+
+
+
+/*
+ * Can use 3 pointers
+ * - prev = null
+ * - curr = head
+ * - next = null
+ * While curr is not null:
+ * - store next node of curr in next
+ * - reverse the next ponter of curr to point to prev
+ * - move prev to curr (for next iteration)
+ * - move curr to next
+ * Update the head
+ * - at end of loop, prev will be the new head of the reversed list
+ * Return prev as New Head
+
+
+ 
+ */
