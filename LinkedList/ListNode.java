45a15864d51c06dc3dd23db5effc3eefa7854a4f
Anthea Ip <antheaip964@gmail.com>
2024-11-13 20:01:21 -0500

    Java

--- LinkedList/ListNode.java
+++ LinkedList/ListNode.java
@@ -6,21 +6,41 @@ public class ListNode {
         ListNode() {}
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        public static void main(String[] args) {
+            // Create a sample linked list 1 -> 2 -> 3 -> 4 -> 5
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+            System.out.println("Original list: ");
+            RevLinkList.printList(head);
+           
+    
+            // Reverse it
+            RevLinkList revLinkList = new RevLinkList();
+            ListNode reversed = revLinkList.reverseList(head);
+            System.out.println("Reversed: ");
+            RevLinkList.printList(reversed);
+        }
     }
+
 
 class RevLinkList {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         // ListNode current = head;
 
-        while (current != null) {
-            ListNode next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
         }
         return prev;
     }
+
     public static void printList(ListNode head) {
         ListNode temp = head;
         while (temp != null) {
@@ -29,23 +49,9 @@ class RevLinkList {
         }
         System.out.println();
     }
-
-    public static void main(String[] args) {
-        // Create a sample linked list 1 -> 2 -> 3 -> 4 -> 5
-        ListNode.head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        System.out.println("Original list: ");
-        printList(head);
-
-        // Reverse it
-        ListNode reversed = Solution.reverseList(head);
-        System.out.println("Reversed: ");
-        printList(reversed);
-    }
 }
+
+    
 
 
 
