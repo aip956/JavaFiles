de7e91fe6d7c4ac9ce344699ef94b9badcc04050
Anthea Ip <antheaip964@gmail.com>
2024-11-18 19:53:58 -0500

    Java

--- LinkedList/linkedListCycle.java
+++ LinkedList/linkedListCycle.java
@@ -2,7 +2,19 @@ import java.util.*;
 
 public class linkedListCycle {
     public boolean hasCycle(ListNode head) {
-        
+        if (head == null || head.next == null) {return false;}
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next // move slow by one
+            fast = fast.next.next; // move fast by 2
+
+            if (slow == fast) {
+                return true; // Cycle detected
+            }
+        }   
+        return false; // no cycle
     }
 
     public static void printList(ListNode list) {
@@ -17,6 +29,6 @@ public class linkedListCycle {
     public static void main(String[] args) {
         ListNode head = new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4))));
         int pos = 1;
-        
+
     }
 }
