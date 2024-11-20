d5cf96a2fc33f68850110820c38bdd93955917c7
Anthea Ip <antheaip964@gmail.com>
2024-11-19 19:39:36 -0500

    Java

--- LinkedList/linkedListCycle.java
+++ LinkedList/linkedListCycle.java
@@ -17,17 +17,11 @@ public class linkedListCycle {
         return false; // no cycle
     }
 
-    public static void printList(ListNode list) {
-        ListNode temp = head;
-        while (temp != null) {
-            System.out.print(temp.val + " ");
-            temp = temp.next;
-        }
-        System.out.println();
-    }
+    
 
     public static void main(String[] args) {
         ListNode head = new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4))));
+        // ListNode head = new ListNode(3, ListNode(2, ListNode(0, ListNode(4, ListNode(2)))));
         int pos = 1;
 
     }
