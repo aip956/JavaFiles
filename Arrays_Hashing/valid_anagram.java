fb0908f661c660644397fff5d635b89de778cf04
Anthea Ip <antheaip964@gmail.com>
2024-11-29 14:48:36 -0800

    Java

--- Arrays_Hashing/valid_anagram.java
+++ Arrays_Hashing/valid_anagram.java
@@ -2,7 +2,19 @@ import java.util.*;
 
 class valid_anagram {
     public static boolean isAnagram(String s, String t) {
-        
+        if (s.length() != t.length()) return false;
+        // Make a freq map
+        HashMap <Character, Integer> sFreq = new HashMap<>();
+        for (char letter : s.toCharArray()) {
+            sFreq.put(letter, sFreq.getOrDefault(letter, 0) + 1);
+        }
+        for (char letter : t.toCharArray()) {
+            // if letter not in sFreq, return false
+            if (!sFreq.containsKey(letter)) return false;
+            
+        }
+
+        return true;
     }
 
     public static void main(String[] args){
@@ -11,3 +23,11 @@ class valid_anagram {
         System.out.println(isAnagram(s, t));
     }
 }
+
+
+/*
+ * https://leetcode.com/problems/valid-anagram/description/
+ * check if = length()
+ * Make a freq map of s
+ * 
+ */
