a8c8dd627f75bb164d58fa68877b05a0955349b8
Anthea Ip <antheaip964@gmail.com>
2024-11-29 15:47:17 -0800

    Java

---
+++ Arrays_Hashing/GroupAnagram.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class GroupAnagram {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
