dcb5fe8ec19eaac3507c6ffea84c466ef57e69dc
Anthea Ip <antheaip964@gmail.com>
2024-11-29 14:40:25 -0800

    Java

---
+++ Arrays_Hashing/valid_anagram.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class valid_anagram {
+    public static boolean isAnagram(String s, String t) {
+        
+    }
+
+    public static void main(String[] args){
+        String s = "rat";
+        String t = "car";
+        System.out.println(isAnagram(s, t));
+    }
+}
