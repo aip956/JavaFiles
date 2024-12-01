d2a0587b43ea8e7193412f1f1758515f6476ec9c
Anthea Ip <antheaip964@gmail.com>
2024-11-30 17:44:56 -0800

    Java

--- Arrays_Hashing/GroupAnagram.java
+++ Arrays_Hashing/GroupAnagram.java
@@ -1,7 +1,10 @@
 import java.util.*;
 class GroupAnagram {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            
+        }
     }
 
     public static void main(String[] args) {
