107dc46acabeba0a755263e977a09e5b7c025ee4
Anthea Ip <antheaip964@gmail.com>
2024-12-01 18:08:11 -0800

    Java

--- Arrays_Hashing/TopK.java
+++ Arrays_Hashing/TopK.java
@@ -1,2 +1,36 @@
 import java.util.*;
 
+class Solution {
+    public static int[] topKFrequent(int[] nums, int k) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+    }
+}
+
+/*
+Freq map of each element of array
+PQueue to store the elements in order of freq
+If the size of PQ > k, remove the least frequent element
+Add remaining elements to the result array
+return result array
+
+ * Given an integer array nums and an integer k, 
+ * return the k most frequent elements. 
+ * You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+ */
