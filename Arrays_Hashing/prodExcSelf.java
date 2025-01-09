7aadec343b69b74e1f0a75febf7ca5d7be67404c
Anthea Ip <antheaip964@gmail.com>
2025-01-08 21:20:05 -0500

    Arrays

--- Arrays_Hashing/prodExcSelf.java
+++ Arrays_Hashing/prodExcSelf.java
@@ -1,7 +1,12 @@
 class prodExcSelf {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
-
+        int prefix =  1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        
         return result;
     }
 
