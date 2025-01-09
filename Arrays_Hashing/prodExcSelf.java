07e29c68e2785693eadac5e3d822cefc1f42fc95
Anthea Ip <antheaip964@gmail.com>
2025-01-08 21:23:28 -0500

    Arrays

--- Arrays_Hashing/prodExcSelf.java
+++ Arrays_Hashing/prodExcSelf.java
@@ -1,20 +1,26 @@
+import java.util.Arrays;
+
 class prodExcSelf {
-    public int[] productExceptSelf(int[] nums) {
+    public static int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
         int prefix =  1;
         for (int i = 0; i < nums.length; i++) {
             result[i] = prefix;
             prefix *= nums[i];
         }
-        
+        int postfix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= postfix;
+            postfix *= nums[i];
+        }
         return result;
     }
 
 
     public static void main(String[] args) {
-        int nums = {1, 2, 3, 4};
-        result = productExceptSelf(nums);
-        System.out.println("Result: " + result);
+        int[] nums = {1, 2, 3, 4};
+        int[] output = productExceptSelf(nums);
+        System.out.println("Result: " + Arrays.toString(output));
     }
 }
 
