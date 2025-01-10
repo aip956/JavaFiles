a5eb74ab69cf5069f77aebeed5664237a12256ea
Anthea Ip <antheaip964@gmail.com>
2025-01-09 20:44:26 -0500

    Arrays

--- Arrays_Hashing/valid.java
+++ Arrays_Hashing/valid.java
@@ -1,8 +1,20 @@
+import java.util.HashMap;
+
 class valid {
     public static boolean isAnagram(String s, String t) {
         // create hash of s
         // subtract each letter in t
+        HashMap <Character, Integer> sFreq = new HashMap<>();
+        // add s chars to sFreq
+        for (Character c : s.toCharArray()) {
+            sFreq.put(c, sFreq.getOrDefault(c, 0) + 1);
+        }
+        // minus t chars
+        for (Character c : t.toCharArray()) {
+            sFreq.put(c, sFreq.getOrDefault(sFreq, 0) - 1);
+        }
         
+        return false;
     }
 
     public static void main(String[] args) {
@@ -12,6 +24,9 @@ class valid {
 }
 
 /*
+frequency hash of s
+subtract each char in t
+
 
  * Given two strings s and t, return true if t is an 
 anagram of s, and false otherwise.
