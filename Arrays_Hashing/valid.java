5c77c73bc0734eeb59bbf72838e3144df162d8bb
Anthea Ip <antheaip964@gmail.com>
2025-01-09 20:22:28 -0500

    Arrays

---
+++ Arrays_Hashing/valid.java
@@ -0,0 +1,16 @@
+class valid {
+    public static boolean isAnagram(String s, String t) {
+        // create hash of s
+        // subtract each letter in t
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+/*
+ * 
+ */
