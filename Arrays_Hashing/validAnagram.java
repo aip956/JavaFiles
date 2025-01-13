9c77c582450a1decdf18bab2fbef579b40449571
Anthea Ip <antheaip964@gmail.com>
2025-01-12 20:30:44 -0500

    Arrays

--- Arrays_Hashing/validAnagram.java
+++ Arrays_Hashing/validAnagram.java
@@ -1,12 +1,17 @@
 class valid {
     public static boolean isAnagram(String s, String t) {
+        // make freq map of s
+        // for each char in t, check if in s
+        // create count, subtract 
 
         return true;
     }
 
 
     public static void main(String[] args) {
+        String s = "abc";
+        String t = "abd";
+        System.out.println(isAnagram(s, t));
 
-        
     }
 }
