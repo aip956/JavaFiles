37f0921bc36079f9442cc0a26e0c5526f03471f2
Anthea Ip <antheaip964@gmail.com>
2025-01-13 19:14:20 -0500

    Arrays

--- Arrays_Hashing/valid_An.java
+++ Arrays_Hashing/valid_An.java
@@ -3,6 +3,12 @@ import java.util.HashMap;
 class valid_An {
     public static boolean validAnagram(String s, String t) {
 
+        return true;
     }
 
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abce";
+        System.out.println("Result: " + validAnagram(s, t));
+    }
 }
