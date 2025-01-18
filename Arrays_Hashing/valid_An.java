20c43eb086b83f6432fa76256e1057ee404b55e0
Anthea Ip <antheaip964@gmail.com>
2025-01-18 11:23:02 -0500

    Arrays

--- Arrays_Hashing/valid_An.java
+++ Arrays_Hashing/valid_An.java
@@ -2,20 +2,7 @@ import java.util.HashMap;
 
 class valid_An {
     public static boolean validAnagram(String s, String t) {
-        // create hash
-        HashMap<Character, Integer> sFreq = new HashMap<>();
-        for (Character c : s.toCharArray()) {
-            sFreq.put(c, sFreq.getOrDefault(c, 0)+ 1);
-        }
-        // subtract t from hash
-        for (Character c : t.toCharArray()) {
-            
-            if (!sFreq.containsKey(c)) return false;
-            int count = sFreq.get(c);
-            sFreq.put(c, count - 1);
-        }
-
-        return true;
+        
     }
 
     public static void main(String[] args) {
