a0a96fcbe18ee321c80b8e1cbc79cb68487eba9d
Anthea Ip <antheaip964@gmail.com>
2025-01-18 11:28:01 -0500

    Arrays

--- Arrays_Hashing/valid_An.java
+++ Arrays_Hashing/valid_An.java
@@ -2,7 +2,21 @@ import java.util.HashMap;
 
 class valid_An {
     public static boolean validAnagram(String s, String t) {
-        
+        if (s.length() != t.length()) return false;
+        // make s freq map
+        HashMap <Character, Integer> sFreq = new HashMap<>();
+        for (Character c : s.toCharArray()) {
+            sFreq.put(c, sFreq.getOrDefault(sFreq, 0) + 1);
+        }
+
+        // subtract each char c; define count
+        // is c in sfreq? yes: add c, count-1 to sfreq
+        for (Character c : t.toCharArray()) {
+            int count = 0;
+            if (!sFreq.containsKey(t)) return false;
+            
+        }
+
     }
 
     public static void main(String[] args) {
