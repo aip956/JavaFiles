849ba5dafd2f0b65ef291655849f18755f1c2c08
Anthea Ip <antheaip964@gmail.com>
2025-01-28 21:45:31 -0500

    Dynamic

--- Dynamic/maxSubArray.java
+++ Dynamic/maxSubArray.java
@@ -1,9 +1,14 @@
 
 class maxSubArray {
     public static int maxSubArray(int[] nums) {
-        
+        // {-2,1,-3,4,-1,2,1,-5,4}
+        // for right ind, max subarr = max of num[i] vs. num[i-1]
+        int maxGlob = nums[0];
+        int maxLoc = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            
+        }
 
-        
         return 0;
     }
 
