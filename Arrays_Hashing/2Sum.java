3f2eacbb56d7b5377cd7eef9ae79a0223aba9d02
Anthea Ip <antheaip964@gmail.com>
2025-01-28 21:25:34 -0500

    Dynamic

---
+++ Arrays_Hashing/2Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(twoSum(nums, target));
+
+
+    }
+}
---
+++ Dynamic/maxSubArray.java
@@ -0,0 +1,15 @@
+
+class maxSubArray {
+    public static int maxSubArray(int[] nums) {
+        
+
+        
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+}
