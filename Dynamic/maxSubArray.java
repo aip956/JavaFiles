b1b14c042ea0cac739dd28747cef541c53209224
Anthea Ip <antheaip964@gmail.com>
2025-01-28 22:15:30 -0500

    Dynamic

--- Dynamic/maxSubArray.java
+++ Dynamic/maxSubArray.java
@@ -2,19 +2,29 @@
 class maxSubArray {
     public static int maxSubArray(int[] nums) {
         // {-2,1,-3,4,-1,2,1,-5,4}
-        /* mL[0] = -2, mL[1]= 1 or -1 = 1
-        mL[2] = -3 or (-3 + 1), mG=-2
+        /* mL[0] = -2, 
+        mL[1]= 1 or -1 = 1, mG=1
+        mL[2] = -3 or (-3 + 1) = -2, mG=-1
         mL[3]= 4 or (-2 + 4) = 4, mG=4
+
+        i   0   1   2   3   4   5   6  7  8  
+        L   -2  1   -2  4   3   5   6  1  5 
+        G   -2  1   1   4   4   5   6  6  6 
+
          * 
          */
         // for right ind, max subarr = max of num[i] vs. num[i-1]
         int maxGlob = nums[0];
         int maxLoc = nums[0];
+        System.out.println("Loc: " + maxLoc + " , Glob: " + maxGlob);
         for (int i = 1; i < nums.length; i++) {
-            maxLoc = Math.max(nums[i], maxLoc);
+            System.out.println("i: " + i);
+            System.out.println("nums[i]: " + nums[i]);
+            maxLoc = Math.max(nums[i], nums[i]+maxLoc);
+            System.out.println("maxLoc: " + maxLoc);
             maxGlob = Math.max(maxLoc, maxGlob);
+            System.out.println("maxGlob: " + maxGlob);
         }
-
         return maxGlob;
     }
 
