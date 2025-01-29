197a3046f1b6f15de1e44736bfa165d9d9d02892
Anthea Ip <antheaip964@gmail.com>
2025-01-28 22:22:26 -0500

    Dynamic

---
+++ Dynamic/maxProdSubarray.java
@@ -0,0 +1,12 @@
+
+class maxProdSubarray {
+    public static int maxProduct(int[] nums) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,-2,4};
+        System.out.println(maxProduct(nums));
+    }
+}
