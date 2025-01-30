f7f54d53ccbf2fda7d30731815d37073a851c8f9
Anthea Ip <antheaip964@gmail.com>
2025-01-29 20:16:40 -0500

    Dynamic

--- Dynamic/climbing.java
+++ Dynamic/climbing.java
@@ -2,11 +2,35 @@
 class climbing {
     public static int climbStairs(int n) {
         if (n == 0 || n == 1) return 1;
-        else return climbStairs(n - 1) + climbStairs(n - 2);
+        // else return climbStairs(n - 1) + climbStairs(n - 2); // this is slow
+        // int[] nums = new int[n + 1];
+        // nums[0] = 1;
+        // nums[1] = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     nums[i] = nums[i-1] + nums[i-2];
+        // }
+        // return nums[n];
+
+        int n_first = 1;
+        int n_sec = 1;
+        int sum_i = 0;
+        for (int i = 2; i <= n; i++) {
+            System.out.println("i: " + i);
+            System.out.println("n_first: " + n_first);
+            System.out.println("n_sec: " + n_sec);
+            sum_i = n_first + n_sec; // 2
+            System.out.println("sum_i: " + sum_i);
+            n_first = n_sec; // 1
+            n_sec = sum_i; // 2
+        }
+        return sum_i;
     }
 
+    // but we only need to save 2 numbers in the array
+
+
     public static void main(String[] args) {
-        int n = 3;
+        int n = 5;
         System.out.println(climbStairs(n));
     }
 }
