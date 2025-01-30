05c66923ff9379f6b8b0ca89a210c52f1f816c24
Anthea Ip <antheaip964@gmail.com>
2025-01-30 17:22:00 -0500

    Dynamic

---
+++ Dynamic/houseRob2.java
@@ -0,0 +1,14 @@
+class houseRob2 {
+    public static int rob(int[] nums) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 3, 2};
+        System.out.println(rob(nums1));
+        System.out.println();
+        int[] nums2 = {1, 2, 3, 1};
+        System.out.println(rob(nums2));
+
+    }
+}
