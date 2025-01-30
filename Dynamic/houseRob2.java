2fc3e57297aa6c4a7d2c1b69e04d206553ce3750
Anthea Ip <antheaip964@gmail.com>
2025-01-30 18:08:31 -0500

    Dynamic

--- Dynamic/houseRob2.java
+++ Dynamic/houseRob2.java
@@ -27,29 +27,25 @@ class houseRob2 {
     }
 
     private static int robHelper(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
         
         for (int i = 2; i < nums.length; i++) {
             dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
-            // System.out.println("i: " + i);
-            // System.out.println("nums[i]: " + nums[i]);
-            // System.out.println("dp_i2: " + dp[i-2]);
-            // System.out.println("dp_i1: " + dp[i-1]);
-            // System.out.println("dp_i: " + dp[i]);
-
         }
         return dp[nums.length - 1];
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {2, 3, 2};
-        System.out.println(rob(nums1));
-        System.out.println();
-        int[] nums2 = {1, 2, 3, 1};
-        System.out.println(rob(nums2));
-        System.out.println();
+        // int[] nums1 = {2, 3, 2};
+        // System.out.println(rob(nums1));
+        // System.out.println();
+        // int[] nums2 = {1, 2, 3, 1};
+        // System.out.println(rob(nums2));
+        // System.out.println();
         int[] nums3 = {2, 7, 3, 1, 4, 2, 1, 8};
         System.out.println(rob(nums3));
     }
