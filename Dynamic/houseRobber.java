0652335af00039b7d7ec0f3a6d3398b73aaf5242
Anthea Ip <antheaip964@gmail.com>
2025-01-30 17:11:14 -0500

    Dynamic

--- Dynamic/houseRobber.java
+++ Dynamic/houseRobber.java
@@ -1,37 +1,36 @@
 class houseRobber {
     public static int rob(int[] nums) {
-        // if (nums.length == 0 || nums == null) return 0;
-        // if (nums.length == 1) return nums[0];
-        // if (nums.length == 2) return Math.max(nums[0], nums[1]);
-
-        // int[] dp = new int[nums.length];
-        // dp[0] = nums[0];
-        // dp[1] = Math.max(nums[0], nums[1]);
-        // for (int i = 2; i < nums.length; i++) {
-        //     dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
-        // }
-        // return dp[nums.length - 1];
-
         /*
          * total loot of n-2 + n, vs. n-1; nums=2,7,9,3,1
          * total array: [2, 7, ]
          */
-        if (nums.length < 2) return nums[0];
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
-
-            System.out.println("i: " + i);
-            System.out.println("nums[i]: " + nums[i]);
-            System.out.println("dp_i2: " + dp[i-2]);
-            System.out.println("dp_i1: " + dp[i-1]);
-            System.out.println("dp_i: " + dp[i]);
-
+        // without the whole array
+        if (nums.length < 2 || nums == null) return nums[0];
+        if (nums.length == 1) return nums[0];
+        int prev2 = 0; // dp[i-2]
+        int prev1 = 0;
+        for (int num : nums) {
+            int temp = prev1;
+            prev1 = Math.max(prev1, prev2 + num);
+            prev2 = temp;
         }
-        return dp[nums.length - 1];
+
+    return prev1;
+        // int[] dp = new int[nums.length];
+        // dp[0] = nums[0];
+        // dp[1] = Math.max(nums[0], nums[1]);
+        
+        // for (int i = 2; i < nums.length; i++) {
+        //     dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+
+        //     System.out.println("i: " + i);
+        //     System.out.println("nums[i]: " + nums[i]);
+        //     System.out.println("dp_i2: " + dp[i-2]);
+        //     System.out.println("dp_i1: " + dp[i-1]);
+        //     System.out.println("dp_i: " + dp[i]);
+
+        // }
+        // return dp[nums.length - 1];
     }
 
     public static void main(String[] args) {
