f08cc527a05fd91e54b69c1b594b9524bb266ea4
Anthea Ip <antheaip964@gmail.com>
2025-01-29 19:20:32 -0500

    Dynamic

---
+++ Dynamic/climbing.java
@@ -0,0 +1,11 @@
+
+class climbing {
+    public static int climbStairs(int n) {
+        
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(climbStairs(n));
+    }
+}
