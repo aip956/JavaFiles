017e1f26ad07cf7add6d7ea3d0210b5756e996d0
Anthea Ip <antheaip964@gmail.com>
2025-01-30 17:00:13 -0500

    Dynamic

--- Dynamic/houseRobber.java
+++ Dynamic/houseRobber.java
@@ -13,16 +13,20 @@ class houseRobber {
         // return dp[nums.length - 1];
 
         /*
-         * total loot of n-2 + n, vs. n-1
+         * total loot of n-2 + n, vs. n-1; nums=2,7,9,3,1
+         * total array: [2, 7, ]
          */
-        int n_2 = nums[0];
-        int n_1 = nums[1];
-        int total_loot = 0;
+        if (nums.length < 2) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
         for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
             int loot = Math.max(n_2 + nums[i], n_1);
             total_loot = Math.max(loot, total_loot);
-            n_2 = n_1;
-            n_1 = nums[i-1];
+            n_2 = total_loot;
+            n_1 = nums[i];
             System.out.println("i: " + i);
             System.out.println("nums[i]: " + nums[i]);
             System.out.println("loot: " + loot);
