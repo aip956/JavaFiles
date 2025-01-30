9c94c5f35b512f6593ba8f15e35375d408a98002
Anthea Ip <antheaip964@gmail.com>
2025-01-29 19:33:21 -0500

    Dynamic

--- Dynamic/climbing.java
+++ Dynamic/climbing.java
@@ -1,7 +1,8 @@
 
 class climbing {
     public static int climbStairs(int n) {
-        
+        if (n == 0 || n == 1) return 1;
+        else return climbStairs(n - 1) + climbStairs(n - 2);
     }
 
     public static void main(String[] args) {
@@ -9,3 +10,14 @@ class climbing {
         System.out.println(climbStairs(n));
     }
 }
+
+
+/*
+ * num_ways(n) = num_ways(n-1) + num_ways(n-2)
+ * n=0, nw = 1
+ * n=1, nw = 1
+ * n=2, nw = 2
+ * n=3, nw = 2+1=3
+ * n=4, nw=3+2=5
+ * count from top stair = 0
+ */
