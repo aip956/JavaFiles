af1c11518caa45d00e284aad76545f4b0382b534
Anthea Ip <antheaip964@gmail.com>
2025-02-01 17:29:08 -0500

    Backtrack

--- BackTrack/comboSum.java
+++ BackTrack/comboSum.java
@@ -1,8 +1,26 @@
+import java.util.*;
+
 class Solution {
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        return [[1], [1]];
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, 0, new ArrayList<>());
+        return result;
     }
+
+    private void backtrack(int[] candidates, int target, int s, List<Integer> current, List<List<Integer>> result) {
+        if(target == 0) {
+        result.add(new ArrayList<>(current));
+        return;
+    }
+    if (target < 0) return;
+    for(int i = s; i < candidates.length; i++) {
+        if(candidates[i] < target) break;
+        current.add(candidates[i]);
+        backtrack(candidates, target - candidates[i], i, current, result);
+        current.remove(current.size() - 1);
+    }
+
 
     public static void main(String[] args) {
         int[] cands1 = {2, 3, 6, 7}; // [[2,2,3], [7]]
