aa946c9541d4943bfe4484874111c97e93309a06
Anthea Ip <antheaip964@gmail.com>
2025-02-02 11:01:35 -0500

    Dynamic

---
+++ Dynamic/int2.java
@@ -0,0 +1,24 @@
+public class int2 {
+    public static int[] sub_arr (int[] arr) {
+        // base case
+        if (arr == null || arr.length < 1) return null;
+        
+
+
+
+        // create sub_arr
+        int[] sub_arr = new int[];
+
+        return sub_arr;
+    }
+
+}
+
+
+
+/*
+* given array of integers, find the subarray with greatest sum
+ * return subarray
+ * pos int, 0, neg int
+ * {1,2,3,-5,0,1,2} => {1,2,3}
+ */
