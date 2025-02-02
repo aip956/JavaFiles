348b0af1a5457940e5edfaa5349da24076b5aa2e
Anthea Ip <antheaip964@gmail.com>
2025-02-02 13:20:33 -0500

    Dynamic

--- Dynamic/int4.java
+++ Dynamic/int4.java
@@ -3,9 +3,26 @@ given array of integers, find the subarray with greatest sum
 return subarray {-2,1,-3,4,-1,2,1,-5,4}; => {4,-1,2,1}
  */
 
-public class int3 {
+public class int4 {
     public static int[] findMaxSub(int[] arr) {
-
+        if (arr == null || arr.length < 1) return null;
+        int mLoc = arr[0], mGl=arr[0];
+        int start_i = 0, end_i = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int n = arr[i];
+            int old_Gl = mGl;
+            if (n > n + mLoc) start_i = i;
+            mLoc = Math.max(n, n + mLoc);
+            mGl = Math.max (mLoc, mGl);
+            if (mLoc > old_Gl) end_i = i;
+        }
+        // make the subarry
+        int len = end_i - start_i + 1;
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            result[i] = arr[start_i];
+            start_i++;
+        }
         return result;
     }
 
@@ -19,6 +36,7 @@ public class int3 {
                 System.out.print(",");
             }
         }
+        System.out.println();
     }
 
 
