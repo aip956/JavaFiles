8a4d4b08fdac193431279401b523a0f2a31c36ea
Anthea Ip <antheaip964@gmail.com>
2025-02-02 11:06:52 -0500

    Dynamic

--- Dynamic/int2.java
+++ Dynamic/int2.java
@@ -2,7 +2,10 @@ public class int2 {
     public static int[] sub_arr (int[] arr) {
         // base case
         if (arr == null || arr.length < 1) return null;
-        
+        //  mLoc = max (n, n+mLoc)
+        //  mGl = max (mLoc, mGl)
+        //  start_i
+        // end_i for subarray
 
 
 
@@ -21,4 +24,11 @@ public class int2 {
  * return subarray
  * pos int, 0, neg int
  * {1,2,3,-5,0,1,2} => {1,2,3}
+ * i=0,n=1,mL=1,mG=1
+ * i=1,n=2,mL=2 or 2+1=3,mG=3
+ * i=2,n=3,mL=3 or 3+3=6,mG=6
+ * i=3,n=-5,mL=-5 or -5+6=1,mG=6
+ * mLoc = max (n, n+mLoc)
+ * mGl = max (mLoc, mGl)
+ * start_i, end_i for subarray
  */
