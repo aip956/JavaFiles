97e8265fcecb9621af72b7b085e07ca489af83f0
Anthea Ip <antheaip964@gmail.com>
2025-02-16 22:19:00 -0500

    Greedy

---
+++ Intervals/meetingRooms.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Interval {
+    int start;
+    int end;
+
+    Interval(int s, int e) {
+        this.start = s;
+        this.end = e;
+    }
+}
+
+class meetingRooms {
+    public static boolean canAttend(List<Interval> intervals) {
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<Interval> intervals = Arrays.asList(
+            new Interval(0,30),
+            new Interval(5,10),
+            new Interval(15,20)
+        );
+        System.out.println("Can Attend: " + canAttend(intervals));
+    }
+}
+
+
+
+
+
+
+
+
+/*
+ * Meeting Rooms
+Given an array of meeting time interval objects consisting 
+of start and end times [[start_1,end_1],[start_2,end_2],...] 
+(start_i < end_i), determine if a person could add all 
+meetings to their schedule without any conflicts.
+
+Example 1:
+
+Input: intervals = [(0,30),(5,10),(15,20)]
+
+Output: false
+Explanation:
+
+(0,30) and (5,10) will conflict
+(0,30) and (15,20) will conflict
+Example 2:
+
+Input: intervals = [(5,8),(9,15)]
+
+Output: true
+Note:
+
+(0,8),(8,10) is not considered a conflict at 8
+Constraints:
+
+0 <= intervals.length <= 500
+0 <= intervals[i].start < intervals[i].end <= 1,000,000
+
+ */
