5375e5c11f2393c02077b9a2391ecd429ba21138
Anthea Ip <antheaip964@gmail.com>
2025-02-16 21:58:27 -0500

    Greedy

--- Greedy/reachingPts.java
+++ Greedy/reachingPts.java
@@ -1,8 +1,22 @@
 class reachingPts {
     public static boolean reachingPoints(int sx, int sy, int tx, int ty) {
-
-
-
+        while (tx >= sx && ty >= sy) {
+            if (tx == sx && ty == sy) return true;
+        }
+        if (tx > ty) {
+            if (ty > sy) {
+                tx %= ty;
+            } else {
+                return (tx - sx) % ty == 0;
+            }
+        } else {
+            if (tx > sx) {
+                ty %= tx;
+            } else {
+                return (ty - sy) % tx == 0;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -12,8 +26,6 @@ class reachingPts {
         int ty = 5;
         System.out.println("Output: " + reachingPoints(sx, sy, tx, ty));
     }
-
-
 }
 
 
@@ -32,7 +44,7 @@ through some operations, or false otherwise.
 
 The allowed operation on some point (x, y) is to convert it to 
 either (x, x + y) or (x + y, y).
-
+While target >= source
 Source = (2, 5), Target = (19, 12)
 If target's x > y, we had to use the (x+y, y)
 if tx > ty: tx -= ty
@@ -42,8 +54,12 @@ else ty > tx: ty -=tx
 7, 5: tx>ty -> tx -= ty -> 2, 5
 But this is time long
 Could instead:
-if tx > ty: tx %= ty -> 19 %= 12 = 7
-else ty > tx: ty -=tx
+tx > ty: tx %= ty -> 19 %= 12 = 7,12
+ty > tx: ty %= tx -> 7,5
+tx > ty: tx %= ty -> 2,5
+but % can return 0
+so if the target x or y == source x or y, focus on the other
+
 
 Example 1:
 Input: sx = 1, sy = 1, tx = 3, ty = 5
