cb35ec4c23c4bf0ed8bb06f03099704ed96a88f8
Anthea Ip <antheaip964@gmail.com>
2025-02-17 12:27:38 -0500

    Intervals

--- Greedy/minAbsDiff.java
+++ Greedy/minAbsDiff.java
@@ -5,17 +5,26 @@ class minAbsDiff {
     public static List<List<Integer>> minimumAbsDifference(int[] arr) {
         // error check
         if (arr == null || arr.length == 0) return null;
-
         // sort
         Arrays.sort(arr);
+        // declare result
+        List<List<Integer>> result = new ArrayList<>();
         // iterate
-        int diff = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+            int diff = arr[i] - arr[i - 1];
+            if (diff < minDiff) {
+                minDiff = diff;
+                result.clear(); // remove previous pairs
+                result.add(Arrays.asList(arr[i - 1],arr[i]));
+            } else if (diff == minDiff) {
+                minDiff = diff;
+                result.add(Arrays.asList(arr[i - 1],arr[i]));
+            }
         }
-
-
         // return
+        return result;
     }
 
     public static void main(String[] args) {
