9909009d403ff40aee7b95788f8c37b4a47cadd5
Anthea Ip <antheaip964@gmail.com>
2025-02-18 20:22:09 -0500

    Intervals

---
+++ Intervals/maxNumClients.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+/*
+ * There is a client-server architecture with n clients
+ * and one server. Each client starts its interaction 
+ * with the server at the second start[i] and stops at
+ * the second end[i]. The maximum traffic is defined as
+ * the max number of concurrent interactions with the 
+ * server.
+ * 
+ * Find the earliest time at which the amx number of clients
+ * are interacting with the server.
+ */
