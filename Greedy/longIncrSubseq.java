6a9c31697a56e5d627cf33e12b0ee75b4e367dd0
Anthea Ip <antheaip964@gmail.com>
2025-03-01 14:13:58 -0500

    Greedy

---
+++ Greedy/longIncrSubseq.java
@@ -0,0 +1,18 @@
+
+
+
+
+/*
+https://docs.google.com/document/d/1DI6uTzq4A-yUx9qzQaG-E4hG2s9puuTqlZnoN-zVANE/edit?tab=t.0
+
+Longest Increasing Subsequence - Room 6
+Given an array of integers, find the length of the longest increasing subsequence (LIS).
+A subsequence is a sequence derived by deleting some elements (without reordering).
+Example:
+vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+
+
+Output: 
+4 (The corresponding LIS is {2, 3, 7, 101}).
+
+ */
