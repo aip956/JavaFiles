d211c2ffd8f85b0b211bd8bb707fc6e270ecdbbc
Anthea Ip <antheaip964@gmail.com>
2025-03-02 11:40:56 -0500

    Greedy

--- Greedy/longIncrSubseq.java
+++ Greedy/longIncrSubseq.java
@@ -3,6 +3,13 @@
 
 
 /*
+use list sub to store smallest possible increasing subseq
+    the list does not store the LIS, but helps determine its length
+Iterate through the input array and process each element greedily
+    if the current number is greater than the last element in sub, apppend it to sub (greedy choice)
+    o/w, find the first element in sub that is >= current num and replace (using binary search)
+    this keeps sub as small as possible ensuring efficient updates
+the length of sub at end is length of lis
 https://docs.google.com/document/d/1DI6uTzq4A-yUx9qzQaG-E4hG2s9puuTqlZnoN-zVANE/edit?tab=t.0
 
 Longest Increasing Subsequence - Room 6
