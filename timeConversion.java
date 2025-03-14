88759f37f510ad1efb40955275d8c0d2bd97685a
Anthea Ip <antheaip964@gmail.com>
2025-03-14 17:35:52 -0400

    time conversion

--- timeConversion.java
+++ timeConversion.java
@@ -2,13 +2,31 @@ import java.util.*;
 
 class timeConversion {
     public static String timeConversion(String s) {
-        String AMPM = s.substring(8)
+        String AMPM = s.substring(8);
         // Convert hour from string to integer
+        // get hour integer; do if statement to add or not
         String timePart = s.substring(0, 8);
+        String hour_st = s.substring(0, 2);
+        int hour = Integer.parseInt(hour_st);
+        System.out.println("Hour: " + hour);
+        System.out.println("AMPM: " + AMPM);
+        if (AMPM == "PM") {
+            if (hour != 12) {
+                hour += 12;
+                System.out.println("converted hour: " + hour);
+            }
+        else {
+            if (hour == 12) {
+                hour -= 12;
+                System.out.println("Converted hour: " + hour);
+            }
+        }
+        }
+        // if AM, 
+        // if 12, subtract 12
+        // else return number
 
-        
-
-        return time;
+        return "abc";
     
         }
 
@@ -27,7 +45,7 @@ class timeConversion {
 /*
  * https://www.hackerrank.com/challenges/time-conversion/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
  * Given a time in -hour AM/PM format, convert it to military (24-hour) time.
-
+Integer.parseInt(string)
 Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
 - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
