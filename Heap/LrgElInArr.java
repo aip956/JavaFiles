8a0c6b63ad04a1f025cdb7e5e0b62753e8db1b0f
Anthea Ip <antheaip964@gmail.com>
2025-03-22 15:13:49 -0400

    Heap

---
+++ Heap/LrgElInArr.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class LrgElInArr {
+    public int findKthLargest(int[] nums, int k) {
+        
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+
+/* 
+Use pq, size n
+but need to manually limit pq to n; pq.size()
+
+Larges Element in Array
+ * Find the Kth Largest Element in an Array
+https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+Problem Statement:
+ Given an unsorted array of integers, find the kth largest element 
+in the array. Do not use built-in sorting functions.
+
+Requirements:
+The algorithm should run in O(n log n) or better time complexity.
+
+
+The function should return an integer representing the kth largest element.
+
+
+Example Cases:
+Input: nums = [3,2,1,5,6,4], k = 2  
+Output: 5  
+
+Input: nums = [7,10,4,3,20,15], k = 3  
+Output: 7 
+
+
+ */
