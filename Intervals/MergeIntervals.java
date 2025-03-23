e400dd0d7c1eb9a99ff27aea7f46a79de7c21ccb
Anthea Ip <antheaip964@gmail.com>
2025-03-22 20:05:59 -0400

    Intervals

---
+++ Intervals/MergeIntervals.java
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+/*
+sort by first element
+
+ * Merge Overlapping Intervals
+Problem Statement:
+ You are given a list of intervals where each interval is represented as [start, end]. Your task is to merge all overlapping intervals and return a new list of non-overlapping intervals.
+Requirements:
+The intervals should be sorted in ascending order based on their start times before processing.
+
+
+The algorithm should run in O(n log n) time complexity due to sorting.
+
+
+Example Cases:
+
+Input: [[1,3],[2,6],[8,10],[15,18]]  
+Output: [[1,6],[8,10],[15,18]]  
+Explanation: The intervals [1,3] and [2,6] overlap, so they are merged into [1,6].
+
+Input: [[1,4],[4,5]]  
+Output: [[1,5]]
+
+ */
