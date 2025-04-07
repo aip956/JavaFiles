d4eb5d7d425a1a74f40f241b8c99976aa75fe070
Anthea Ip <antheaip964@gmail.com>
2025-04-07 15:02:26 -0700

    Implementation

---
+++ implementation/LibraryFine.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class LibraryFine {
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        // Write your code here
+    
+        }
+
+    public static void main(String[] args) {
+        // Returned:
+        int d1 = 9;
+        int m1 = 6;
+        int y1 = 2015;
+        // Checked out:
+        int d2 = 6;
+        int m2 = 6; 
+        int y2 = 2015;
+    }
+
+
+    
+}
