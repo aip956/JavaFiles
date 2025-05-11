f0a3c836368aa82e8ea2cf663cb287c353772632
Anthea Ip <antheaip964@gmail.com>
2025-05-11 13:21:59 -0400

    Repeated string

---
+++ implementation/repeatedString.java
@@ -0,0 +1,25 @@
+
+
+class repeatedString {
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long repeatedString(String s, long n) {
+    // Write your code here
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abcac";
+        long n = 10;
+        System.out.println(repeatedString(s, n));
+    }
+
+}
